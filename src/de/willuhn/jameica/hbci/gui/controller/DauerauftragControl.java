366e29381e456b827b65d10fc81305079dc10c12
willuhn <willuhn>
2004-07-16 00:07:31 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/hbci/gui/controller/DauerauftragControl.java
+++ src/de/willuhn/jameica/hbci/gui/controller/DauerauftragControl.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/controller/DauerauftragControl.java,v $
- * $Revision: 1.1 $
- * $Date: 2004/07/13 23:08:37 $
+ * $Revision: 1.2 $
+ * $Date: 2004/07/16 00:07:31 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -18,12 +18,14 @@ import de.willuhn.datasource.rmi.DBIterator;
 import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.gui.Part;
 import de.willuhn.jameica.gui.formatter.CurrencyFormatter;
+import de.willuhn.jameica.gui.formatter.Formatter;
 import de.willuhn.jameica.gui.parts.TablePart;
 import de.willuhn.jameica.gui.views.AbstractView;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.Settings;
 import de.willuhn.jameica.hbci.gui.views.DauerauftragNeu;
 import de.willuhn.jameica.hbci.rmi.Dauerauftrag;
+import de.willuhn.util.Logger;
 
 /**
  * Controller fuer Dauer-Auftraege.
@@ -69,6 +71,24 @@ public class DauerauftragControl extends AbstractTransferControl {
 		table.addColumn(i18n.tr("BLZ des Empfängers"),"empfaenger_blz");
 		table.addColumn(i18n.tr("Name des Empfängers"),"empfaenger_name");
 		table.addColumn(i18n.tr("Betrag"),"betrag", new CurrencyFormatter("",HBCI.DECIMALFORMAT));
+		table.addColumn(i18n.tr("aktiv"),"aktiv",new Formatter()
+    {
+      public String format(Object o)
+      {
+      	if (o == null)
+      		return "nein";
+      	try {
+      		int i = ((Integer) o).intValue();
+      		return i == 0 ? i18n.tr("nein") : i18n.tr("ja");
+      	}
+      	catch (Exception e)
+      	{
+      		Logger.error("error while formatting attribute",e);
+      		return "unbekannt";
+      	}
+      }
+    });
+		table.addColumn(i18n.tr("Turnus"),"turnus_id");
 		return table;
 	}
 
@@ -98,6 +118,9 @@ public class DauerauftragControl extends AbstractTransferControl {
 
 /**********************************************************************
  * $Log: DauerauftragControl.java,v $
+ * Revision 1.2  2004/07/16 00:07:31  willuhn
+ * *** empty log message ***
+ *
  * Revision 1.1  2004/07/13 23:08:37  willuhn
  * @N Views fuer Dauerauftrag
  *
