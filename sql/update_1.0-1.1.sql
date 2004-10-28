5a74d0305b154b42f8f278b0a6a67d3ddb4899be
willuhn <willuhn>
2004-10-28 22:13:07 +0000

    *** keyword substitution change ***

--- sql/update_1.0-1.1.sql
+++ sql/update_1.0-1.1.sql
@@ -20,8 +20,17 @@ ALTER CREATE TABLE turnus (
   zeiteinheit int(1) NOT NULL,
   intervall int(2) NOT NULL,
   tag int(2) NOT NULL,
-  bezeichnung varchar(255) NOT NULL,
   initial int(1),
+  UNIQUE (id),
+  PRIMARY KEY (id)
+);
+
+ALTER CREATE TABLE restriction (
+  id NUMERIC default UNIQUEKEY('restriction'),
+  konto_id int(4) NOT NULL,
+  jobname varchar(1000) NOT NULL,
+  parameter varchar(255) NOT NULL,
+  value varchar(255),
   UNIQUE (id),
   PRIMARY KEY (id)
 );
@@ -29,21 +38,22 @@ ALTER CREATE TABLE turnus (
 
 ALTER TABLE dauerauftrag ADD CONSTRAINT fk_konto4 FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
 ALTER TABLE dauerauftrag ADD CONSTRAINT fk_turnus FOREIGN KEY (turnus_id) REFERENCES turnus (id) DEFERRABLE;
+ALTER TABLE restriction ADD CONSTRAINT fk_konto5 FOREIGN KEY (konto_id) REFERENCES restriction (id) DEFERRABLE;
 
-INSERT INTO turnus (zeiteinheit,intervall,tag,bezeichnung,initial)
-  VALUES (2,1,1,'Monatlich, zum 1. des Monats',1);
+INSERT INTO turnus (zeiteinheit,intervall,tag,initial)
+  VALUES (2,1,1,1);
 
-INSERT INTO turnus (zeiteinheit,intervall,tag,bezeichnung,initial)
-  VALUES (2,1,15,'Monatlich, zum 15. des Monats',1);
+INSERT INTO turnus (zeiteinheit,intervall,tag,initial)
+  VALUES (2,1,15,1);
 
-INSERT INTO turnus (zeiteinheit,intervall,tag,bezeichnung,initial)
-  VALUES (2,3,1,'Vierteljährlich, zum 1. des Monats',1);
+INSERT INTO turnus (zeiteinheit,intervall,tag,initial)
+  VALUES (2,3,1,1);
 
-INSERT INTO turnus (zeiteinheit,intervall,tag,bezeichnung,initial)
-  VALUES (2,6,1,'Halbjährlich, zum 1. des Monats',1);
+INSERT INTO turnus (zeiteinheit,intervall,tag,initial)
+  VALUES (2,6,1,1);
 
-INSERT INTO turnus (zeiteinheit,intervall,tag,bezeichnung,initial)
-  VALUES (2,12,1,'Jährlich, zum 1. des Monats',1);
+INSERT INTO turnus (zeiteinheit,intervall,tag,initial)
+  VALUES (2,12,1,1);
 
-INSERT INTO turnus (zeiteinheit,intervall,tag,bezeichnung,initial)
-  VALUES (1,1,1,'Wöchentlich, Montag',1);
+INSERT INTO turnus (zeiteinheit,intervall,tag,initial)
+  VALUES (1,1,1,1);
