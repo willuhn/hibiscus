4b561b9a8b423001de752cace17fba8657cc0d1f
willuhn <willuhn>
2006-04-21 09:26:35 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/hbci/gui/dialogs/ImportDialog.java
+++ src/de/willuhn/jameica/hbci/gui/dialogs/ImportDialog.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/dialogs/ImportDialog.java,v $
- * $Revision: 1.5 $
- * $Date: 2006/04/20 08:44:21 $
+ * $Revision: 1.6 $
+ * $Date: 2006/04/21 09:26:35 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -39,6 +39,7 @@ import de.willuhn.jameica.hbci.io.IORegistry;
 import de.willuhn.jameica.hbci.io.Importer;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.jameica.system.BackgroundTask;
+import de.willuhn.jameica.system.OperationCanceledException;
 import de.willuhn.jameica.system.Settings;
 import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
@@ -98,11 +99,11 @@ public class ImportDialog extends AbstractDialog
 				doImport();
 			}
 		},null,true);
-		buttons.addButton(i18n.tr("Schliessen"), new Action()
+		buttons.addButton(i18n.tr("Abbrechen"), new Action()
 		{
 			public void handleAction(Object context) throws ApplicationException
 			{
-				close();
+				throw new OperationCanceledException();
 			}
 		});
   }
@@ -314,6 +315,9 @@ public class ImportDialog extends AbstractDialog
 
 /**********************************************************************
  * $Log: ImportDialog.java,v $
+ * Revision 1.6  2006/04/21 09:26:35  willuhn
+ * *** empty log message ***
+ *
  * Revision 1.5  2006/04/20 08:44:21  willuhn
  * @C s/Childs/Children/
  *
