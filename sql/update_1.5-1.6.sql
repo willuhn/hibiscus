c6e9844990da9ce6e4a3653ec3d8d1954a61b450
willuhn <willuhn>
2006-05-11 20:34:16 +0000

    @B fehleranfaellige SQL-Updates entfernt

--- sql/update_1.5-1.6.sql
+++ sql/update_1.5-1.6.sql
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------
 -- $Source: /cvsroot/hibiscus/hibiscus/sql/Attic/update_1.5-1.6.sql,v $
--- $Revision: 1.1 $
--- $Date: 2006/04/27 22:26:16 $
+-- $Revision: 1.2 $
+-- $Date: 2006/05/11 20:34:16 $
 -- $Author: willuhn $
 -- $Locker:  $
 -- $State: Exp $
@@ -11,14 +11,24 @@
 --
 ------------------------------------------------------------------------
 
--- Vergessene Constraints
-ALTER TABLE ueberweisung ADD CONSTRAINT fk_konto_usb FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
-ALTER TABLE slastschrift ADD CONSTRAINT fk_konto_slast FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
-ALTER TABLE umsatz ADD CONSTRAINT fk_konto_ums FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
-ALTER TABLE lastschrift ADD CONSTRAINT fk_konto_last FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
+
+-- Verlorengegangene Constraints
+-- Vorher sicherheitshalber loeschen (falls einige schon existieren)
+--ALTER TABLE ueberweisung DROP CONSTRAINT fk_konto_usb;
+--ALTER TABLE slastschrift DROP CONSTRAINT fk_konto_slast;
+--ALTER TABLE umsatz DROP CONSTRAINT fk_konto_ums;
+--ALTER TABLE lastschrift DROP CONSTRAINT fk_konto_last;
+
+--ALTER TABLE ueberweisung ADD CONSTRAINT fk_konto_usb FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
+--ALTER TABLE slastschrift ADD CONSTRAINT fk_konto_slast FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
+--ALTER TABLE umsatz ADD CONSTRAINT fk_konto_ums FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
+--ALTER TABLE lastschrift ADD CONSTRAINT fk_konto_last FOREIGN KEY (konto_id) REFERENCES konto (id) DEFERRABLE;
 
 ------------------------------------------------------------------------
 -- $Log: update_1.5-1.6.sql,v $
+-- Revision 1.2  2006/05/11 20:34:16  willuhn
+-- @B fehleranfaellige SQL-Updates entfernt
+--
 -- Revision 1.1  2006/04/27 22:26:16  willuhn
 -- *** empty log message ***
 --
--- src/de/willuhn/jameica/hbci/HBCI.java
+++ src/de/willuhn/jameica/hbci/HBCI.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/HBCI.java,v $
- * $Revision: 1.89 $
- * $Date: 2006/04/27 22:26:16 $
+ * $Revision: 1.90 $
+ * $Date: 2006/05/11 20:34:16 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -146,31 +146,31 @@ public class HBCI extends AbstractPlugin
     try {
 			Application.getCallback().getStartupMonitor().setStatusText("hibiscus: checking database integrity");
 
-      ////////////////////////////////////////////////////////////////////////////
-      // Damit wir die Updates nicht immer haendisch nachziehen muessen, rufen wir
-      // bei einem Fehler das letzte Update-Script nochmal auf.
-			if (!Application.inClientMode())
-      {
-        try
-        {
-          de.willuhn.jameica.system.Settings s = getResources().getSettings();
-          double size = s.getDouble("sql-update-size",-1);
-          
-          File f = new File(getResources().getPath() + "/sql/update_1.5-1.6.sql");
-          
-          long length = f.length();
-          if (length != size)
-          {
-            getDatabase().executeSQLScript(f);
-            s.setAttribute("sql-update-size",(double)f.length());
-          }
-        }
-        catch (Exception e2)
-        {
-          e2.printStackTrace();
-        }
-      }
-      ////////////////////////////////////////////////////////////////////////////
+//      ////////////////////////////////////////////////////////////////////////////
+//      // Damit wir die Updates nicht immer haendisch nachziehen muessen, rufen wir
+//      // bei einem Fehler das letzte Update-Script nochmal auf.
+//			if (!Application.inClientMode())
+//      {
+//        try
+//        {
+//          de.willuhn.jameica.system.Settings s = getResources().getSettings();
+//          double size = s.getDouble("sql-update-size",-1);
+//          
+//          File f = new File(getResources().getPath() + "/sql/update_1.5-1.6.sql");
+//          
+//          long length = f.length();
+//          if (length != size)
+//          {
+//            s.setAttribute("sql-update-size",(double)f.length());
+//            getDatabase().executeSQLScript(f);
+//          }
+//        }
+//        catch (Exception e2)
+//        {
+//          Logger.error("unable to execute sql update script",e2);
+//        }
+//      }
+//      ////////////////////////////////////////////////////////////////////////////
 
       checkConsistency();
 		}
@@ -382,6 +382,9 @@ public class HBCI extends AbstractPlugin
 
 /**********************************************************************
  * $Log: HBCI.java,v $
+ * Revision 1.90  2006/05/11 20:34:16  willuhn
+ * @B fehleranfaellige SQL-Updates entfernt
+ *
  * Revision 1.89  2006/04/27 22:26:16  willuhn
  * *** empty log message ***
  *
