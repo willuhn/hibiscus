f815e48e5fdc30fae991b4310ab5e4dfaf50b06b
jost <jost>
2006-05-15 20:14:51 +0000

    Ausgabe -> PDF-Ausgabe

--- src/de/willuhn/jameica/hbci/gui/views/KontoauszugList.java
+++ src/de/willuhn/jameica/hbci/gui/views/KontoauszugList.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/views/KontoauszugList.java,v $
- * $Revision: 1.1 $
- * $Date: 2006/05/14 19:53:09 $
+ * $Revision: 1.2 $
+ * $Date: 2006/05/15 20:14:51 $
  * $Author: jost $
  * $Locker:  $
  * $State: Exp $
@@ -30,49 +30,50 @@ import de.willuhn.util.I18N;
  */
 public class KontoauszugList extends AbstractView
 {
-	private I18N i18n = null;
+  private I18N i18n = null;
 
-	private KontoauszugControl control = null;
+  private KontoauszugControl control = null;
 
-	public KontoauszugList()
-	{
-		super();
-		this.i18n = Application.getPluginLoader().getPlugin(HBCI.class)
-				.getResources().getI18N();
+  public KontoauszugList()
+  {
+    super();
+    this.i18n = Application.getPluginLoader().getPlugin(HBCI.class)
+        .getResources().getI18N();
 
-	}
+  }
 
-	/**
-	 * @see de.willuhn.jameica.gui.AbstractView#bind()
-	 */
-	public void bind() throws Exception
-	{
-		GUI.getView().setTitle(i18n.tr("Kontoauszug"));
+  /**
+   * @see de.willuhn.jameica.gui.AbstractView#bind()
+   */
+  public void bind() throws Exception
+  {
+    GUI.getView().setTitle(i18n.tr("Kontoauszug"));
 
-		control = new KontoauszugControl(this);
+    control = new KontoauszugControl(this);
 
-		LabelGroup settings = new LabelGroup(getParent(), i18n.tr("Filter"));
+    LabelGroup settings = new LabelGroup(getParent(), i18n.tr("Filter"));
 
-		settings.addLabelPair(i18n.tr("Konto"), control.getKontoAuswahl());
-		settings.addLabelPair(i18n.tr("Start-Datum"), control.getStart());
-		settings.addLabelPair(i18n.tr("Ende-Datum"), control.getEnd());
+    settings.addLabelPair(i18n.tr("Konto"), control.getKontoAuswahl());
+    settings.addLabelPair(i18n.tr("Start-Datum"), control.getStart());
+    settings.addLabelPair(i18n.tr("Ende-Datum"), control.getEnd());
 
-		ButtonArea buttons = new ButtonArea(getParent(), 1);
-		buttons.addButton(i18n.tr("Ausgabe"), new Action()
-		{
-			public void handleAction(Object context) throws ApplicationException
-			{
-				control.startReport();
-			}
-		});
+    ButtonArea buttons = new ButtonArea(getParent(), 1);
+    buttons.addButton(i18n.tr("PDF-Ausgabe"), new Action()
+    {
+      public void handleAction(Object context) throws ApplicationException
+      {
+        control.startReport();
+      }
+    });
 
-	}
+  }
 
 }
 
 /*******************************************************************************
  * $Log: KontoauszugList.java,v $
- * Revision 1.1  2006/05/14 19:53:09  jost
- * Prerelease Kontoauszug-Report
- * Revision 1.4 2006/01/18 00:51:00
+ * Revision 1.2  2006/05/15 20:14:51  jost
+ * Ausgabe -> PDF-Ausgabe
+ * Revision 1.1 2006/05/14 19:53:09 jost
+ * Prerelease Kontoauszug-Report Revision 1.4 2006/01/18 00:51:00
  ******************************************************************************/
