fc4a9236c4c644f5eac9ce03d27f2563a4f76127
willuhn <willuhn>
2006-07-05 23:29:29 +0000

    @B bug 174

--- src/de/willuhn/jameica/hbci/gui/dialogs/PINDialog.java
+++ src/de/willuhn/jameica/hbci/gui/dialogs/PINDialog.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/dialogs/PINDialog.java,v $
- * $Revision: 1.15 $
- * $Date: 2006/01/08 23:23:19 $
+ * $Revision: 1.16 $
+ * $Date: 2006/07/05 23:29:29 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -15,12 +15,21 @@ package de.willuhn.jameica.hbci.gui.dialogs;
 import java.rmi.RemoteException;
 import java.security.NoSuchAlgorithmException;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.kapott.hbci.manager.HBCIUtils;
 import org.kapott.hbci.passport.HBCIPassport;
 
 import de.willuhn.jameica.gui.GUI;
+import de.willuhn.jameica.gui.Part;
 import de.willuhn.jameica.gui.dialogs.PasswordDialog;
-import de.willuhn.jameica.gui.util.SWTUtil;
+import de.willuhn.jameica.gui.input.CheckboxInput;
+import de.willuhn.jameica.gui.util.Color;
+import de.willuhn.jameica.gui.util.Container;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.HBCIProperties;
 import de.willuhn.jameica.hbci.Settings;
@@ -42,7 +51,11 @@ public class PINDialog extends PasswordDialog {
 	private I18N i18n;
 	private HBCIPassport passport;
 
-  private String walletKey = null;
+  private String walletKey       = null;
+  
+  private CheckboxInput checkPin = null;
+  private Label checkPinText     = null;
+  private String checkPinComment = null;
 
   /**
    * ct.
@@ -54,6 +67,7 @@ public class PINDialog extends PasswordDialog {
   public PINDialog(HBCIPassport passport) {
 
     super(PINDialog.POSITION_CENTER);
+    setSize(550,SWT.DEFAULT);
     this.passport = passport;
 
     // BUGZILLA 71 http://www.willuhn.de/bugzilla/show_bug.cgi?id=71
@@ -94,31 +108,73 @@ public class PINDialog extends PasswordDialog {
     if (s != null)
     {
       setTitle(i18n.tr("PIN-Eingabe. Konto: {0}",s));
-      
-      String text  = i18n.tr("Bitte geben Sie Ihre PIN ein. Konto: {0}",s);
-      
-//      text += "\n\n";
-//
-//      // BUGZILLA 174
-//      text += i18n.tr("Hinweis: Falls sich Ihre PIN geändert hat, dann löschen Sie bitte die Prüfsumme unter\n" +
-//                      "Plugins>Hibiscus>Einstellungen>Gespeicherte Prüfsummen löschen");
-      setText(text);
-
+      setText(i18n.tr("Bitte geben Sie Ihre PIN ein. Konto: {0}",s));
     }
     else
     {
       setTitle(i18n.tr("PIN-Eingabe"));
       setText(i18n.tr("Bitte geben Sie Ihre PIN ein."));
     }
-		setSideImage(SWTUtil.getImage("password.gif"));
+    
+    this.checkPinComment = i18n.tr("Hinweis: Falls sich Ihre PIN geändert hat, dann löschen Sie bitte die\n" +
+                                   "Check-Summe unter \"Plugins->Hibiscus->Einstellungen->Gespeicherte Prüfsummen löschen\"");
   }
 
+  /**
+   * @see de.willuhn.jameica.gui.dialogs.PasswordDialog#extend(de.willuhn.jameica.gui.util.Container)
+   */
+  protected void extend(Container container) throws Exception
+  {
+    // BUGZILLA 174
+    this.checkPin = new CheckboxInput(Settings.getCheckPin());
+    container.addCheckbox(checkPin,i18n.tr("PIN-Eingabe via Check-Summe prüfen"));
+
+    Part p = new Part() {
+      public void paint(Composite parent) throws RemoteException
+      {
+        checkPinText = new Label(parent,SWT.WRAP);
+        checkPinText .setBackground(Color.BACKGROUND.getSWTColor());
+        if (Settings.getCheckPin())
+          checkPinText.setText(checkPinComment);
+        else
+          checkPinText .setText("\n\n\n");
+        checkPinText.setForeground(Color.COMMENT.getSWTColor());
+        checkPinText .setLayoutData(new GridData(GridData.FILL_BOTH));
+        // Workaround fuer Windows, weil dort mehrzeilige
+        // Labels nicht korrekt umgebrochen werden.
+        checkPinText .addControlListener(new ControlAdapter() {
+          public void controlResized(ControlEvent e)
+          {
+            checkPinText .setSize(checkPinText .computeSize(checkPinText.getSize().x,SWT.DEFAULT));
+          }
+        });
+      }
+    };
+    container.addPart(p);
+  }
+  
 	/**
    * @see de.willuhn.jameica.gui.dialogs.PasswordDialog#checkPassword(java.lang.String)
    */
   protected boolean checkPassword(String password)
 	{
-  	// BUGZILLA 28 http://www.willuhn.de/bugzilla/show_bug.cgi?id=28
+    // Aktuellen Wert der Checkbox holen
+    if (this.checkPin != null)
+    {
+      Boolean b = (Boolean)this.checkPin.getValue();
+      
+      // Wir uebernehmen den Wert in die Einstellungen
+      Settings.setCheckPin(b.booleanValue());
+    }
+
+    // BUGZILLA 174
+    // Erstmal Text resetten
+    if (Settings.getCheckPin() && this.checkPinText != null && !this.checkPinText.isDisposed())
+    {
+      this.checkPinText.setText("\n\n\n");
+    }
+
+    // BUGZILLA 28 http://www.willuhn.de/bugzilla/show_bug.cgi?id=28
 		if (password == null || password.length() < HBCIProperties.HBCI_PIN_MINLENGTH || password.length() > HBCIProperties.HBCI_PIN_MAXLENGTH)
 		{
 			String[] s = new String[] {
@@ -178,6 +234,13 @@ public class PINDialog extends PasswordDialog {
 
       setErrorText(i18n.tr("PIN falsch.") + " " + getRetryString());
 
+      // BUGZILLA 174
+      if (this.checkPinText != null && !this.checkPinText.isDisposed())
+      {
+        this.checkPinText.setText(checkPinComment);
+        this.checkPinText.redraw();
+      }
+
       return false;
 
     }
@@ -205,6 +268,9 @@ public class PINDialog extends PasswordDialog {
 
 /**********************************************************************
  * $Log: PINDialog.java,v $
+ * Revision 1.16  2006/07/05 23:29:29  willuhn
+ * @B bug 174
+ *
  * Revision 1.15  2006/01/08 23:23:19  willuhn
  * *** empty log message ***
  *
