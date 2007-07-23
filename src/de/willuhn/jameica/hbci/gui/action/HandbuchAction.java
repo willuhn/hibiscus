a9138ca962b7681ae7eb315e97072a2866db58da
jost <jost>
2007-07-23 17:46:45 +0000

    Neu: Anzeige des Handbuches

---
+++ src/de/willuhn/jameica/hbci/gui/action/HandbuchAction.java
@@ -0,0 +1,56 @@
+/**********************************************************************
+ * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/action/HandbuchAction.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2007/07/23 17:46:45 $
+ * $Author: jost $
+ * $Locker:  $
+ * $State: Exp $
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * All rights reserved
+ *
+ **********************************************************************/
+package de.willuhn.jameica.hbci.gui.action;
+
+import java.io.File;
+
+import de.willuhn.jameica.gui.Action;
+import de.willuhn.jameica.gui.GUI;
+import de.willuhn.jameica.gui.internal.action.Program;
+import de.willuhn.jameica.hbci.HBCI;
+import de.willuhn.jameica.messaging.StatusBarMessage;
+import de.willuhn.jameica.plugin.AbstractPlugin;
+import de.willuhn.jameica.system.Application;
+import de.willuhn.util.ApplicationException;
+
+public class HandbuchAction implements Action
+{
+  public void handleAction(Object context) throws ApplicationException
+  {
+    GUI.getDisplay().asyncExec(new Runnable()
+    {
+      public void run()
+      {
+        try
+        {
+          AbstractPlugin p = Application.getPluginLoader()
+              .getPlugin(HBCI.class);
+          new Program().handleAction(new File(p.getResources().getPath()
+              + "/doc/hibiscus_handbuch.pdf"));
+        }
+        catch (ApplicationException ae)
+        {
+          Application.getMessagingFactory().sendMessage(
+              new StatusBarMessage(ae.getLocalizedMessage(),
+                  StatusBarMessage.TYPE_ERROR));
+        }
+      }
+    });
+  }
+}
+/*******************************************************************************
+ * $Log: HandbuchAction.java,v $
+ * Revision 1.1  2007/07/23 17:46:45  jost
+ * Neu: Anzeige des Handbuches
+ *
+ ******************************************************************************/
