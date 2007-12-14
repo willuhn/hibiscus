dac640eec9f69bbb6d11deee0f9374678f41a49a
willuhn <willuhn>
2007-12-14 17:06:36 +0000

    @B Bug 518

--- src/de/willuhn/jameica/hbci/gui/controller/UmsatzDetailControl.java
+++ src/de/willuhn/jameica/hbci/gui/controller/UmsatzDetailControl.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/controller/UmsatzDetailControl.java,v $
- * $Revision: 1.31 $
- * $Date: 2007/12/03 10:00:27 $
+ * $Revision: 1.32 $
+ * $Date: 2007/12/14 17:06:36 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -226,10 +226,9 @@ public class UmsatzDetailControl extends AbstractControl {
       try
       {
         String name = HBCIUtils.getNameForBLZ(blz);
+        blz = i18n.tr("[BLZ: {0}]",blz);
         if (name != null && name.length() > 0)
-          blz = name;
-        else
-          blz = i18n.tr("BLZ") + ": " + blz;
+          blz = name + " " + blz; // BUGZILLA 518
       }
       catch (Exception e)
       {
@@ -258,8 +257,7 @@ public class UmsatzDetailControl extends AbstractControl {
     }
     else
     {
-      empfaengerBlz= new LabelInput(blz);
-      empfaengerBlz.setComment(HBCIUtils.getNameForBLZ(blz));
+      empfaengerBlz = new LabelInput(blz);
     }
     return empfaengerBlz;
   }
@@ -510,6 +508,9 @@ public class UmsatzDetailControl extends AbstractControl {
 
 /**********************************************************************
  * $Log: UmsatzDetailControl.java,v $
+ * Revision 1.32  2007/12/14 17:06:36  willuhn
+ * @B Bug 518
+ *
  * Revision 1.31  2007/12/03 10:00:27  willuhn
  * @N Umsatz-Kategorien nach Name sortieren, wenn keine Nummer angegeben
  *
