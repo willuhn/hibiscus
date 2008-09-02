af7882717dd9bc232ed9b5d68eb12e04958d0657
willuhn <willuhn>
2008-09-02 18:14:25 +0000

    @N Diagnose-Backup erweitert

--- src/de/willuhn/jameica/hbci/gui/action/BackupCreate.java
+++ src/de/willuhn/jameica/hbci/gui/action/BackupCreate.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/action/BackupCreate.java,v $
- * $Revision: 1.1 $
- * $Date: 2008/01/22 13:34:45 $
+ * $Revision: 1.2 $
+ * $Date: 2008/09/02 18:14:25 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -14,12 +14,23 @@
 package de.willuhn.jameica.hbci.gui.action;
 
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
+
+import de.willuhn.datasource.BeanUtil;
+import de.willuhn.datasource.GenericObject;
 import de.willuhn.datasource.rmi.DBIterator;
 import de.willuhn.datasource.serialize.Writer;
 import de.willuhn.datasource.serialize.XmlWriter;
 import de.willuhn.jameica.gui.Action;
+import de.willuhn.jameica.gui.GUI;
+import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.Settings;
 import de.willuhn.jameica.hbci.server.DauerauftragImpl;
 import de.willuhn.jameica.hbci.server.HibiscusAddressImpl;
@@ -35,69 +46,197 @@ import de.willuhn.jameica.hbci.server.TurnusImpl;
 import de.willuhn.jameica.hbci.server.UeberweisungImpl;
 import de.willuhn.jameica.hbci.server.UmsatzImpl;
 import de.willuhn.jameica.hbci.server.UmsatzTypImpl;
+import de.willuhn.jameica.hbci.server.VerwendungszweckImpl;
+import de.willuhn.jameica.messaging.StatusBarMessage;
+import de.willuhn.jameica.system.Application;
+import de.willuhn.jameica.system.BackgroundTask;
 import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
+import de.willuhn.util.I18N;
+import de.willuhn.util.ProgressMonitor;
 
 /**
+ * Action zum Erstellen eines Komplett-Backups im XML-Format.
  */
 public class BackupCreate implements Action
 {
+  /**
+   * Dateformat, welches fuer den Dateinamen genutzt wird.
+   */
+  public static DateFormat DATEFORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+  private static I18N i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
 
   /**
    * @see de.willuhn.jameica.gui.Action#handleAction(java.lang.Object)
    */
   public void handleAction(Object context) throws ApplicationException
   {
-    Writer writer = null;
+    FileDialog fd = new FileDialog(GUI.getShell(),SWT.SAVE);
+    fd.setFilterPath(System.getProperty("user.home"));
+    fd.setFileName("hibiscus-backup-" + DATEFORMAT.format(new Date()) + ".xml");
+    fd.setFilterExtensions(new String[]{"*.xml"});
+    fd.setText("Bitte wählen Sie die Datei, in der das Backup gespeichert wird");
+    String f = fd.open();
+    if (f == null || f.length() == 0)
+      return;
+    
+    final File file = new File(f);
     try
     {
-      writer = new XmlWriter(new BufferedOutputStream(new FileOutputStream("/tmp/install/backup.xml")));
-      backup(TurnusImpl.class,writer);
-      backup(UmsatzTypImpl.class,writer);
-
-
-      backup(HibiscusAddressImpl.class,writer);
-
-      backup(KontoImpl.class,writer);
-      backup(NachrichtImpl.class,writer);
-      backup(UmsatzImpl.class,writer);
-      
-      backup(DauerauftragImpl.class,writer);
-      backup(LastschriftImpl.class,writer);
-      backup(UeberweisungImpl.class,writer);
-
-      backup(SammelLastschriftImpl.class,writer);
-      backup(SammelLastBuchungImpl.class,writer);
-
-      backup(SammelUeberweisungImpl.class,writer);
-      backup(SammelUeberweisungBuchungImpl.class,writer);
-
-      // Die Protokolle zum Schluss.
-      backup(ProtokollImpl.class,writer);
+      if (file.exists() && !Application.getCallback().askUser(i18n.tr("Datei existiert bereits. Überschreiben?")))
+        return;
+    }
+    catch (ApplicationException ae)
+    {
+      throw ae;
     }
     catch (Exception e)
     {
-      throw new ApplicationException(e.getMessage());
+      Logger.error("error while asking user",e);
+      Application.getMessagingFactory().sendMessage(new StatusBarMessage(i18n.tr("Fehler beim Erstellen der Backup-Datei"), StatusBarMessage.TYPE_ERROR));
+      return;
     }
-    finally
-    {
-      if (writer != null)
+
+    Application.getController().start(new BackgroundTask() {
+      private boolean cancel = false;
+    
+      /**
+       * @see de.willuhn.jameica.system.BackgroundTask#run(de.willuhn.util.ProgressMonitor)
+       */
+      public void run(ProgressMonitor monitor) throws ApplicationException
       {
+        Writer writer = null;
         try
         {
-          writer.close();
-          Logger.info("Backup fertig");
+          Logger.info("creating xml backup to " + file.getAbsolutePath());
+
+          writer = new XmlWriter(new BufferedOutputStream(new FileOutputStream(file)));
+
+          monitor.setStatusText(i18n.tr("Speichere Turnus-Informationen"));
+          backup(TurnusImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          
+          monitor.setStatusText(i18n.tr("Speichere Umsatz-Kategorien"));
+          backup(UmsatzTypImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+
+          monitor.setStatusText(i18n.tr("Speichere Adressbuch"));
+          backup(HibiscusAddressImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          monitor.setStatusText(i18n.tr("Speichere Konten und Systemnachrichten"));
+          backup(KontoImpl.class,writer,monitor);
+          backup(NachrichtImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          monitor.setStatusText(i18n.tr("Speichere Umsätze"));
+          backup(UmsatzImpl.class,writer,monitor);
+          monitor.addPercentComplete(20);
+          
+          monitor.setStatusText(i18n.tr("Speichere Daueraufträge"));
+          backup(DauerauftragImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          monitor.setStatusText(i18n.tr("Speichere Lastschriften"));
+          backup(LastschriftImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          monitor.setStatusText(i18n.tr("Speichere Überweisungen"));
+          backup(UeberweisungImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          monitor.setStatusText(i18n.tr("Speichere Sammel-Lastschriften"));
+          backup(SammelLastschriftImpl.class,writer,monitor);
+          backup(SammelLastBuchungImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          monitor.setStatusText(i18n.tr("Speichere Sammel-Überweisungen"));
+          backup(SammelUeberweisungImpl.class,writer,monitor);
+          backup(SammelUeberweisungBuchungImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          monitor.setStatusText(i18n.tr("Speichere erweiterte Verwendungszwecke"));
+          backup(VerwendungszweckImpl.class,writer,monitor);
+          monitor.addPercentComplete(5);
+
+          // Die Protokolle zum Schluss.
+          monitor.setStatusText(i18n.tr("Speichere Protokolle"));
+          backup(ProtokollImpl.class,writer,monitor);
+          monitor.addPercentComplete(20);
+          
+          // Die Versionstabelle wird nicht mit kopiert
+          
+          monitor.setStatusText("Backup erstellt");
+          monitor.setPercentComplete(100);
+          monitor.setStatus(ProgressMonitor.STATUS_DONE);
         }
-        catch (Exception e) {/*useless*/}
+        catch (Exception e)
+        {
+          throw new ApplicationException(e.getMessage());
+        }
+        finally
+        {
+          if (writer != null)
+          {
+            try
+            {
+              writer.close();
+              Logger.info("backup created");
+            }
+            catch (Exception e) {/*useless*/}
+          }
+        }
       }
-    }
+    
+      /**
+       * @see de.willuhn.jameica.system.BackgroundTask#isInterrupted()
+       */
+      public boolean isInterrupted()
+      {
+        return this.cancel;
+      }
+    
+      /**
+       * @see de.willuhn.jameica.system.BackgroundTask#interrupt()
+       */
+      public void interrupt()
+      {
+        this.cancel = true;
+      }
+    
+    });
   }
   
-  private void backup(Class type, Writer writer) throws Exception
+  /**
+   * Hilfsfunktion.
+   * @param type
+   * @param writer
+   * @param monitor
+   * @throws Exception
+   */
+  private static void backup(Class type, Writer writer, ProgressMonitor monitor) throws Exception
   {
     DBIterator list = Settings.getDBService().createList(type);
+    long count = 1;
     while (list.hasNext())
-      writer.write(list.next());
+    {
+      GenericObject o = null;
+      try
+      {
+        o = list.next();
+        writer.write(o);
+        if (count++ % 200 == 0)
+          monitor.addPercentComplete(1);
+      }
+      catch (Exception e)
+      {
+        Logger.error("error while writing object " + BeanUtil.toString(o) + " - skipping",e);
+        monitor.log("  " + i18n.tr("{0} fehlerhaft ({1}), überspringe",new String[]{BeanUtil.toString(o),e.getMessage()}));
+      }
+    }
   }
 
 }
@@ -105,6 +244,9 @@ public class BackupCreate implements Action
 
 /*********************************************************************
  * $Log: BackupCreate.java,v $
+ * Revision 1.2  2008/09/02 18:14:25  willuhn
+ * @N Diagnose-Backup erweitert
+ *
  * Revision 1.1  2008/01/22 13:34:45  willuhn
  * @N Neuer XML-Import/-Export
  *
--- src/de/willuhn/jameica/hbci/gui/action/BackupRestore.java
+++ src/de/willuhn/jameica/hbci/gui/action/BackupRestore.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/action/BackupRestore.java,v $
- * $Revision: 1.2 $
- * $Date: 2008/04/30 09:01:23 $
+ * $Revision: 1.3 $
+ * $Date: 2008/09/02 18:14:25 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -14,91 +14,156 @@
 package de.willuhn.jameica.hbci.gui.action;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
+
+import de.willuhn.datasource.BeanUtil;
 import de.willuhn.datasource.GenericObject;
 import de.willuhn.datasource.db.AbstractDBObject;
 import de.willuhn.datasource.serialize.ObjectFactory;
 import de.willuhn.datasource.serialize.Reader;
 import de.willuhn.datasource.serialize.XmlReader;
 import de.willuhn.jameica.gui.Action;
+import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.Settings;
 import de.willuhn.jameica.system.Application;
+import de.willuhn.jameica.system.BackgroundTask;
 import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
+import de.willuhn.util.I18N;
+import de.willuhn.util.ProgressMonitor;
 
 /**
+ * Action zum Einspielen eines XML-Backups.
  */
 public class BackupRestore implements Action
 {
+  private static I18N i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
 
   /**
    * @see de.willuhn.jameica.gui.Action#handleAction(java.lang.Object)
    */
   public void handleAction(Object context) throws ApplicationException
   {
-    final ClassLoader loader = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getClassLoader();
+    FileDialog fd = new FileDialog(GUI.getShell(),SWT.OPEN);
+    fd.setFileName("hibiscus-backup-" + BackupCreate.DATEFORMAT.format(new Date()) + ".xml");
+    fd.setFilterExtensions(new String[]{"*.xml"});
+    fd.setText("Bitte wählen Sie die Backup-Datei aus");
+    String f = fd.open();
+    if (f == null || f.length() == 0)
+      return;
+    
+    final File file = new File(f);
+    if (!file.exists())
+      return;
 
-    Reader reader = null;
-    try
-    {
-      InputStream is = new BufferedInputStream(new FileInputStream("/tmp/install/backup.xml"));
-      reader = new XmlReader(is,new ObjectFactory() {
-      
-        public GenericObject create(String type, String id, Map values) throws Exception
-        {
-          AbstractDBObject object = (AbstractDBObject) Settings.getDBService().createObject(loader.loadClass(type),null);
-          object.setID(id);
-          Iterator i = values.keySet().iterator();
-          while (i.hasNext())
-          {
-            String name = (String) i.next();
-            object.setAttribute(name,values.get(name));
-          }
-          return object;
-        }
-      
-      });
-      GenericObject o = null;
-      while ((o = reader.read()) != null)
+    Application.getController().start(new BackgroundTask() {
+      private boolean cancel = false;
+    
+      /**
+       * @see de.willuhn.jameica.system.BackgroundTask#run(de.willuhn.util.ProgressMonitor)
+       */
+      public void run(ProgressMonitor monitor) throws ApplicationException
       {
+        monitor.setStatusText(i18n.tr("Importiere Backup"));
+        Logger.info("importing backup " + file.getAbsolutePath());
+        final ClassLoader loader = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getClassLoader();
+
+        Reader reader = null;
         try
         {
-          ((AbstractDBObject)o).insert();
+          InputStream is = new BufferedInputStream(new FileInputStream(file));
+          reader = new XmlReader(is,new ObjectFactory() {
+          
+            public GenericObject create(String type, String id, Map values) throws Exception
+            {
+              AbstractDBObject object = (AbstractDBObject) Settings.getDBService().createObject(loader.loadClass(type),null);
+              object.setID(id);
+              Iterator i = values.keySet().iterator();
+              while (i.hasNext())
+              {
+                String name = (String) i.next();
+                object.setAttribute(name,values.get(name));
+              }
+              return object;
+            }
+          
+          });
+          
+          long count = 1;
+          GenericObject o = null;
+          while ((o = reader.read()) != null)
+          {
+            try
+            {
+              ((AbstractDBObject)o).insert();
+            }
+            catch (Exception e)
+            {
+              Logger.error("unable to import " + o.getClass().getName() + ":" + o.getID() + ", skipping",e);
+              monitor.log("  " + i18n.tr("{0} fehlerhaft ({1}), überspringe",new String[]{BeanUtil.toString(o),e.getMessage()}));
+            }
+            if (count++ % 100 == 0)
+              monitor.addPercentComplete(1);
+          }
+          
+          monitor.setStatusText("Backup importiert");
+          monitor.setPercentComplete(100);
+          monitor.setStatus(ProgressMonitor.STATUS_DONE);
         }
         catch (Exception e)
         {
-          Logger.error("unable to import " + o.getClass().getName() + ":" + o.getID() + ", skipping",e);
+          Logger.error("error while importing data",e);
+          throw new ApplicationException(e.getMessage());
         }
-      }
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-      throw new ApplicationException(e.getMessage());
-    }
-    finally
-    {
-      if (reader != null)
-      {
-        try
+        finally
         {
-          reader.close();
-          Logger.info("Backup fertig");
+          if (reader != null)
+          {
+            try
+            {
+              reader.close();
+              Logger.info("backup imported");
+            }
+            catch (Exception e) {/*useless*/}
+          }
         }
-        catch (Exception e) {/*useless*/}
       }
-    }
+    
+      /**
+       * @see de.willuhn.jameica.system.BackgroundTask#isInterrupted()
+       */
+      public boolean isInterrupted()
+      {
+        return this.cancel;
+      }
+    
+      /**
+       * @see de.willuhn.jameica.system.BackgroundTask#interrupt()
+       */
+      public void interrupt()
+      {
+        this.cancel = true;
+      }
+    
+    });
   }
 }
 
 
 /*********************************************************************
  * $Log: BackupRestore.java,v $
+ * Revision 1.3  2008/09/02 18:14:25  willuhn
+ * @N Diagnose-Backup erweitert
+ *
  * Revision 1.2  2008/04/30 09:01:23  willuhn
  * @C Fehlerhafte Objekte beim Restore ueberspringen
  *
