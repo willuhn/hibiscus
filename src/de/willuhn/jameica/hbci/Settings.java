410fd73edc43a5baa48d7bec067b92a8e7c494a1
willuhn <willuhn>
2008-12-17 22:53:22 +0000

    @R steinalten Migrationscode entfernt

--- src/de/willuhn/jameica/hbci/Settings.java
+++ src/de/willuhn/jameica/hbci/Settings.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/Settings.java,v $
- * $Revision: 1.53 $
- * $Date: 2008/07/24 09:59:37 $
+ * $Revision: 1.54 $
+ * $Date: 2008/12/17 22:53:22 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -15,7 +15,6 @@ package de.willuhn.jameica.hbci;
 import java.io.File;
 import java.rmi.ConnectException;
 import java.rmi.RemoteException;
-import java.util.Date;
 
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
@@ -341,14 +340,7 @@ public class Settings
   public static Wallet getWallet() throws Exception
   {
 		if (wallet == null)
-    {
       wallet = new Wallet(HBCI.class);
-
-      // BUGZILLA 109 http://www.willuhn.de/bugzilla/show_bug.cgi?id=109
-      // TODO Altes Wallet-Format kann mal raus
-      if (wallet.get("migration") == null)
-        wallet.set("migration",new Date().toString());
-    }
 		return wallet;
   }
 
@@ -395,6 +387,9 @@ public class Settings
 
 /*********************************************************************
  * $Log: Settings.java,v $
+ * Revision 1.54  2008/12/17 22:53:22  willuhn
+ * @R steinalten Migrationscode entfernt
+ *
  * Revision 1.53  2008/07/24 09:59:37  willuhn
  * @C Default-Wert des Auftragslimits erhoeht. 1.000,- waren in der Tat etwas wenig ;)
  *
