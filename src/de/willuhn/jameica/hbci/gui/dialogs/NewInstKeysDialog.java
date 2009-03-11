b92c5639043aaeaa5b4905214796ece18dbb64c9
willuhn <willuhn>
2009-03-11 23:41:52 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/hbci/gui/dialogs/NewInstKeysDialog.java
+++ src/de/willuhn/jameica/hbci/gui/dialogs/NewInstKeysDialog.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/dialogs/NewInstKeysDialog.java,v $
- * $Revision: 1.8 $
- * $Date: 2009/03/11 23:41:36 $
+ * $Revision: 1.9 $
+ * $Date: 2009/03/11 23:41:52 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -75,7 +75,7 @@ public class NewInstKeysDialog extends AbstractDialog
 		{
 			public void handleAction(Object context) throws ApplicationException
 			{
-				choosen = new Boolean(true);
+				choosen = Boolean.TRUE;
 				close();
 			}
 		},null,true);
@@ -102,6 +102,9 @@ public class NewInstKeysDialog extends AbstractDialog
 
 /**********************************************************************
  * $Log: NewInstKeysDialog.java,v $
+ * Revision 1.9  2009/03/11 23:41:52  willuhn
+ * *** empty log message ***
+ *
  * Revision 1.8  2009/03/11 23:41:36  willuhn
  * *** empty log message ***
  *
