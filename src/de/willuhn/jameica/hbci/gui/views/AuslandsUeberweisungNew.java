9cf2e2aea5af84932951088f24d921366d319b57
willuhn <willuhn>
2009-03-17 23:50:08 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/hbci/gui/views/AuslandsUeberweisungNew.java
+++ src/de/willuhn/jameica/hbci/gui/views/AuslandsUeberweisungNew.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/views/AuslandsUeberweisungNew.java,v $
- * $Revision: 1.1 $
- * $Date: 2009/03/13 00:25:12 $
+ * $Revision: 1.2 $
+ * $Date: 2009/03/17 23:50:08 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -50,7 +50,7 @@ public class AuslandsUeberweisungNew extends AbstractView {
 		konten.addLabelPair(i18n.tr("Persönliches Konto"),        control.getKontoAuswahl());
     konten.addHeadline(i18n.tr("Empfänger"));
     konten.addLabelPair(i18n.tr("Name"),                      control.getEmpfaengerName());
-    konten.addLabelPair(i18n.tr("Kontonummer"),               control.getEmpfaengerKonto());    
+    konten.addLabelPair(i18n.tr("IBAN"),                      control.getEmpfaengerKonto());    
     konten.addLabelPair(i18n.tr("Institut"),                  control.getEmpfaengerBank());    
 		konten.addCheckbox(control.getStoreEmpfaenger(),i18n.tr("In Adressbuch übernehmen"));
 
@@ -87,6 +87,9 @@ public class AuslandsUeberweisungNew extends AbstractView {
 
 /**********************************************************************
  * $Log: AuslandsUeberweisungNew.java,v $
+ * Revision 1.2  2009/03/17 23:50:08  willuhn
+ * *** empty log message ***
+ *
  * Revision 1.1  2009/03/13 00:25:12  willuhn
  * @N Code fuer Auslandsueberweisungen fast fertig
  *
