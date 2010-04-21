bc32efb4435a5d4142d568a6eb39ada3fb4c67a0
willuhn <willuhn>
2010-04-21 23:39:15 +0000

    @N Support fuer Offline-Konten

---
+++ src/de/willuhn/jameica/hbci/backend/Backend.java
@@ -0,0 +1,36 @@
+/**********************************************************************
+ * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/backend/Attic/Backend.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2010/04/21 23:39:15 $
+ * $Author: willuhn $
+ *
+ * Copyright (c) by willuhn - software & services
+ * All rights reserved
+ *
+ **********************************************************************/
+
+package de.willuhn.jameica.hbci.backend;
+
+/**
+ * Platzhalter-Interface fuer ein Banking-Backend.
+ * Irgendwann wird Hibiscus neben HBCI vielleicht auch mal
+ * noch andere Protokolle unterstuetzen. Dann wird dieses
+ * Interface tatsaechlich benutzt werden. Derzeit dient es
+ * nur dazu, HBCI-Konten von Offline-Konten zu unterscheiden.
+ * Das Interface ist im Moment nur deshalb nicht als Interface
+ * sondern als Klasse deklariert, weil noch keine sinvolle
+ * Implementierung existiert.
+ */
+public class Backend
+{
+
+}
+
+
+
+/**********************************************************************
+ * $Log: Backend.java,v $
+ * Revision 1.1  2010/04/21 23:39:15  willuhn
+ * @N Support fuer Offline-Konten
+ *
+ **********************************************************************/
