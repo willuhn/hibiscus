d990dbaf1cc8754dee3b5e5b5074df7388855676
willuhn <willuhn>
2010-07-26 09:11:41 +0000

    @N Scripting-Anbindung jetzt via Background-Task und Rueckmeldung via ProgressMonitor

--- src/de/willuhn/jameica/hbci/gui/action/KontoSyncViaScripting.java
+++ src/de/willuhn/jameica/hbci/gui/action/KontoSyncViaScripting.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/action/KontoSyncViaScripting.java,v $
- * $Revision: 1.1 $
- * $Date: 2010/07/25 23:11:59 $
+ * $Revision: 1.2 $
+ * $Date: 2010/07/26 09:11:41 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -21,9 +21,11 @@ import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.rmi.Konto;
 import de.willuhn.jameica.messaging.QueryMessage;
 import de.willuhn.jameica.system.Application;
+import de.willuhn.jameica.system.BackgroundTask;
 import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
 import de.willuhn.util.I18N;
+import de.willuhn.util.ProgressMonitor;
 
 /**
  * Action, die ein Konto via Jameica-Scripting aktualisiert.
@@ -46,34 +48,77 @@ public class KontoSyncViaScripting implements Action
 		if (context == null || !(context instanceof Konto))
 			throw new ApplicationException(i18n.tr("Bitte wählen Sie ein Konto aus"));
 
+    final Konto k = (Konto) context;
 		try {
-			final Konto k = (Konto) context;
 	    if ((k.getFlags() & Konto.FLAG_OFFLINE) != Konto.FLAG_OFFLINE)
 	      throw new ApplicationException(i18n.tr("Bitte wählen Sie ein Offline-Konto aus"));
 
 			if (k.isNewObject())
 				k.store();
+    }
+    catch (RemoteException e)
+    {
+      Logger.error("error while syncing konto",e);
+      GUI.getStatusBar().setErrorText(i18n.tr("Fehler beim Synchronisieren des Kontos"));
+    }
 			
-      final AbstractView currentView = GUI.getCurrentView();
-			Application.getMessagingFactory().getMessagingQueue("jameica.scripting").sendSyncMessage(new QueryMessage("hibiscus_kontoSync",k));
-			
-			// GUI neu laden
-      AbstractView newView = GUI.getCurrentView();
-      if (newView == currentView)
-        currentView.reload();
-		}
-		catch (RemoteException e)
-		{
-			Logger.error("error while syncing konto",e);
-			GUI.getStatusBar().setErrorText(i18n.tr("Fehler beim Synchronisieren des Kontos"));
-		}
+    final AbstractView currentView = GUI.getCurrentView();
+      
+    Application.getController().start(new BackgroundTask() {
+      
+      public void run(ProgressMonitor monitor) throws ApplicationException
+      {
+        monitor.setStatus(ProgressMonitor.STATUS_RUNNING);
+        monitor.setStatusText(i18n.tr("Starte Synchronisierung"));
+        
+        QueryMessage msg = new QueryMessage("hibiscus_kontoSync",new Object[]{k,monitor});
+        Application.getMessagingFactory().getMessagingQueue("jameica.scripting").sendSyncMessage(msg);
+
+        // GUI neu laden
+        AbstractView newView = GUI.getCurrentView();
+        if (newView == currentView)
+          currentView.reload();
+
+        monitor.setPercentComplete(100);
+        Object value = msg.getData();
+        if (value instanceof Throwable)
+        {
+          Throwable t = (Throwable) value;
+          monitor.setStatus(ProgressMonitor.STATUS_ERROR);
+          monitor.setStatusText(i18n.tr("Fehler: {0}",t.getMessage()));
+        }
+        else
+        {
+          monitor.setStatus(ProgressMonitor.STATUS_DONE);
+          monitor.setStatusText(i18n.tr("Synchronisierung erfolgreich beendet"));
+        }
+      }
+      
+      /**
+       * @see de.willuhn.jameica.system.BackgroundTask#isInterrupted()
+       */
+      public boolean isInterrupted()
+      {
+        return false;
+      }
+      
+      /**
+       * @see de.willuhn.jameica.system.BackgroundTask#interrupt()
+       */
+      public void interrupt()
+      {
+      }
+    });
   }
 }
 
 
 /**********************************************************************
  * $Log: KontoSyncViaScripting.java,v $
- * Revision 1.1  2010/07/25 23:11:59  willuhn
+ * Revision 1.2  2010/07/26 09:11:41  willuhn
+ * @N Scripting-Anbindung jetzt via Background-Task und Rueckmeldung via ProgressMonitor
+ *
+ * Revision 1.1  2010-07-25 23:11:59  willuhn
  * @N Erster Code fuer Scripting-Integration
  *
  **********************************************************************/
