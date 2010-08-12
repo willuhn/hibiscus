64118d8a97a473ed9e2ce99c077d6e0e2adfa262
willuhn <willuhn>
2010-08-12 10:13:41 +0000

    @R Der NULL-Check ist unnoetig

--- src/de/willuhn/jameica/hbci/gui/chart/LineChart.java
+++ src/de/willuhn/jameica/hbci/gui/chart/LineChart.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/chart/LineChart.java,v $
- * $Revision: 1.13 $
- * $Date: 2010/08/11 15:48:57 $
+ * $Revision: 1.14 $
+ * $Date: 2010/08/12 10:13:41 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -139,13 +139,6 @@ public class LineChart extends AbstractChart
           Object ovalue = BeanUtil.get(o,dataAttribute);
           Object olabel = BeanUtil.get(o,labelAttribute);
           
-          if (olabel == null || ovalue == null || !(ovalue instanceof Number))
-          {
-            dataLine.add(null);
-            labelLine.add(null);
-            continue;
-          }
-          
           dataLine.add(ovalue);
           labelLine.add(format == null ? olabel : format.format(olabel));
         }
@@ -234,7 +227,10 @@ public class LineChart extends AbstractChart
 
 /*********************************************************************
  * $Log: LineChart.java,v $
- * Revision 1.13  2010/08/11 15:48:57  willuhn
+ * Revision 1.14  2010/08/12 10:13:41  willuhn
+ * @R Der NULL-Check ist unnoetig
+ *
+ * Revision 1.13  2010-08-11 15:48:57  willuhn
  * @B Objekte mit fehlenden Attributen nicht ueberspringen sondern stattdessen NULL im Vector speichern. Sonst kann es zu einer "ChartException: Mismatch (x !=y) in dataset count found in stacked runtime series" kommen, wenn mehrere Linien gezeichnet werden und die eine unterschiedliche Anzahl von Elementen haben
  *
  * Revision 1.12  2009/08/27 13:37:28  willuhn
