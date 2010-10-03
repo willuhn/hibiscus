2258f275cc880b5144a3f88f61675a8307a8c54b
willuhn <willuhn>
2010-10-03 22:07:47 +0000

    @C Dialog sollte natuerlich nur dann angezeigt werden, wenn es noch KEINE Termin-Ueberweisung ist

--- src/de/willuhn/jameica/hbci/gui/action/UeberweisungExecute.java
+++ src/de/willuhn/jameica/hbci/gui/action/UeberweisungExecute.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/action/UeberweisungExecute.java,v $
- * $Revision: 1.13 $
- * $Date: 2010/10/03 21:50:34 $
+ * $Revision: 1.14 $
+ * $Date: 2010/10/03 22:07:47 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -63,7 +63,7 @@ public class UeberweisungExecute implements Action
 			// BUGZILLA 559
 			Date termin = HBCIProperties.startOfDay(u.getTermin());
 			Date now    = HBCIProperties.startOfDay(new Date());
-			if ((termin.getTime() - now.getTime()) >= (24 * 60 * 60 * 1000))
+			if (!u.isTerminUeberweisung() && (termin.getTime() - now.getTime()) >= (24 * 60 * 60 * 1000))
 			{
 			  String q = i18n.tr("Der Termin liegt mindestens 1 Tag in Zukunft.\n" +
 			  		               "Soll der Auftrag stattdessen als bankseitige Termin-Überweisung " +
@@ -120,7 +120,10 @@ public class UeberweisungExecute implements Action
 
 /**********************************************************************
  * $Log: UeberweisungExecute.java,v $
- * Revision 1.13  2010/10/03 21:50:34  willuhn
+ * Revision 1.14  2010/10/03 22:07:47  willuhn
+ * @C Dialog sollte natuerlich nur dann angezeigt werden, wenn es noch KEINE Termin-Ueberweisung ist
+ *
+ * Revision 1.13  2010-10-03 21:50:34  willuhn
  * @N BUGZILLA 559 - wenn der Termin der Ueberweisung mind. 1 Tag in der Zukunft liegt, dann vorschlagen, ihn als bankseitige Termin-Ueberweisung auszufuehren
  *
  * Revision 1.12  2007/07/04 09:16:23  willuhn
