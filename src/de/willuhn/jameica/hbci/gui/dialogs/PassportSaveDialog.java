dea175767b25b21b896dfbff398b54a7cb442193
willuhn <willuhn>
2010-11-22 11:30:51 +0000

    @C Cleanup

--- src/de/willuhn/jameica/hbci/gui/dialogs/PassportSaveDialog.java
+++ src/de/willuhn/jameica/hbci/gui/dialogs/PassportSaveDialog.java
@@ -1,8 +1,8 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/dialogs/Attic/PassportSaveDialog.java,v $
- * $Revision: 1.5 $
- * $Date: 2006/12/24 10:28:16 $
- * $Author: jost $
+ * $Revision: 1.6 $
+ * $Date: 2010/11/22 11:30:51 $
+ * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
  *
@@ -20,18 +20,19 @@ import de.willuhn.util.I18N;
 /**
  * Dialog für die Eingabe eines Passwortes beim Export des Passports.
  */
-public class PassportSaveDialog extends NewPasswordDialog {
-
-	private I18N i18n;
+public class PassportSaveDialog extends NewPasswordDialog
+{
+	private final static I18N i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
+	
   /**
    * ct.
    * @param position Position des Dialogs.
    * @see de.willuhn.jameica.gui.dialogs.AbstractDialog#POSITION_CENTER
    * @see de.willuhn.jameica.gui.dialogs.AbstractDialog#POSITION_MOUSE
    */
-  public PassportSaveDialog(int position) {
+  public PassportSaveDialog(int position)
+  {
     super(position);
-		i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
 
     setTitle(i18n.tr("Passwort-Eingabe"));
     setLabelText(i18n.tr("Ihr Passwort"));
@@ -42,13 +43,7 @@ public class PassportSaveDialog extends NewPasswordDialog {
 
 /**********************************************************************
  * $Log: PassportSaveDialog.java,v $
- * Revision 1.5  2006/12/24 10:28:16  jost
- * Korrektur Tippfehler
- *
- * Revision 1.4  2005/11/14 11:36:58  willuhn
- * @B bug 148
- *
- * Revision 1.3  2005/02/07 22:06:40  willuhn
- * *** empty log message ***
+ * Revision 1.6  2010/11/22 11:30:51  willuhn
+ * @C Cleanup
  *
  **********************************************************************/
