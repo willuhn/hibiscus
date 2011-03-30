1b94911bfaaca277c670efee5b626447993dcc16
willuhn <willuhn>
2011-03-30 08:19:37 +0000

    @C Code-Cleanup

--- src/de/willuhn/jameica/hbci/gui/dialogs/DebugDialog.java
+++ src/de/willuhn/jameica/hbci/gui/dialogs/DebugDialog.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/dialogs/DebugDialog.java,v $
- * $Revision: 1.1 $
- * $Date: 2008/05/06 10:10:56 $
+ * $Revision: 1.2 $
+ * $Date: 2011/03/30 08:19:37 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -19,7 +19,7 @@ import org.eclipse.swt.widgets.Composite;
 import de.willuhn.jameica.gui.Action;
 import de.willuhn.jameica.gui.dialogs.AbstractDialog;
 import de.willuhn.jameica.gui.input.TextAreaInput;
-import de.willuhn.jameica.gui.util.ButtonArea;
+import de.willuhn.jameica.gui.parts.ButtonArea;
 import de.willuhn.jameica.gui.util.Container;
 import de.willuhn.jameica.gui.util.SimpleContainer;
 import de.willuhn.jameica.hbci.HBCI;
@@ -34,7 +34,8 @@ import de.willuhn.util.I18N;
  */
 public class DebugDialog extends AbstractDialog
 {
-  private I18N i18n = null;
+  private final static int WINDOW_WIDTH = 550;
+  private final static I18N i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
 
   /**
    * ct
@@ -43,9 +44,8 @@ public class DebugDialog extends AbstractDialog
   public DebugDialog(int position)
   {
     super(position);
-    this.i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
     this.setTitle(i18n.tr("Diagnose-Informationen"));
-    setSize(560,SWT.DEFAULT);
+    setSize(WINDOW_WIDTH,SWT.DEFAULT);
   }
 
   /**
@@ -61,28 +61,30 @@ public class DebugDialog extends AbstractDialog
    */
   protected void paint(Composite parent) throws Exception
   {
-    StringBuffer sb = new StringBuffer();
     HBCIDBService service = (HBCIDBService) Application.getServiceFactory().lookup(HBCI.class,"database");
     DBSupport driver      = service.getDriver();
 
+    StringBuffer sb = new StringBuffer();
+    sb.append(i18n.tr("JDBC-Treiber: {0}\n",driver.getJdbcDriver()));
+    sb.append(i18n.tr("JDBC-URL: {0}\n",driver.getJdbcUrl()));
+    sb.append(i18n.tr("JDBC-Username: {0}\n",driver.getJdbcUsername()));
+    sb.append(i18n.tr("JDBC-Passwort: {0}\n",driver.getJdbcPassword()));
+    
     Container container = new SimpleContainer(parent);
-    
-    sb.append(i18n.tr("JDBC-Treiber: \"{0}\"\n",driver.getJdbcDriver()));
-    sb.append(i18n.tr("JDBC-URL: \"{0}\"\n",driver.getJdbcUrl()));
-    sb.append(i18n.tr("JDBC-Username: \"{0}\"\n",driver.getJdbcUsername()));
-    sb.append(i18n.tr("JDBC-Passwort: \"{0}\"\n",driver.getJdbcPassword()));
-    
+    container.addHeadline(i18n.tr("Datenbank-Einstellungen"));
     TextAreaInput text = new TextAreaInput(sb.toString());
-    
-    container.addLabelPair("",text);
-    
-    ButtonArea buttons = container.createButtonArea(1);
-    buttons.addButton("   " + i18n.tr("Schlieﬂen") + "   ",new Action() {
+    container.addPart(text);
+
+    ButtonArea buttons = new ButtonArea();
+    buttons.addButton(i18n.tr("Schlieﬂen"),new Action() {
       public void handleAction(Object context) throws ApplicationException
       {
         close();
       }
-    },null,true);
+    },null,true,"window-close.png");
+    container.addButtonArea(buttons);
+    
+    getShell().setMinimumSize(getShell().computeSize(WINDOW_WIDTH,SWT.DEFAULT));
   }
 
 }
@@ -90,6 +92,9 @@ public class DebugDialog extends AbstractDialog
 
 /*********************************************************************
  * $Log: DebugDialog.java,v $
+ * Revision 1.2  2011/03/30 08:19:37  willuhn
+ * @C Code-Cleanup
+ *
  * Revision 1.1  2008/05/06 10:10:56  willuhn
  * @N Diagnose-Dialog, mit dem man die JDBC-Verbindungsdaten (u.a. auch das JDBC-Passwort) ausgeben kann
  *
