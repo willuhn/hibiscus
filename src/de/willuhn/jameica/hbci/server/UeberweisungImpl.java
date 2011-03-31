a9dea4dbd1112649c11bcd5518c9e70233478d49
willuhn <willuhn>
2011-03-31 16:30:58 +0000

    @N BLZ-Check bei Umbuchungen testhalber mal deaktiviert

--- src/de/willuhn/jameica/hbci/server/UeberweisungImpl.java
+++ src/de/willuhn/jameica/hbci/server/UeberweisungImpl.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/server/UeberweisungImpl.java,v $
- * $Revision: 1.44 $
- * $Date: 2010/04/27 11:02:32 $
+ * $Revision: 1.45 $
+ * $Date: 2011/03/31 16:30:58 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -87,10 +87,12 @@ public class UeberweisungImpl extends AbstractBaseUeberweisungImpl implements Ue
         
         // Checken, ob Ziel-BLZ identisch mit Quell-BLZ
         // NULL-Checks brauchen wir hier nicht - das ist bereits in super.insertCheck() gemacht worden
-        String dest = getGegenkontoBLZ();
-        String src  = getKonto().getBLZ();
-        if (!dest.equals(src))
-          throw new ApplicationException(i18n.tr("Umbuchungen sind nur zu einem Konto bei Ihrer eigenen Bank möglich"));
+        // TODO: Siehe http://www.onlinebanking-forum.de/phpBB2/viewtopic.php?p=74505#74505
+        // Nur zum Testen. Morgen wieder einkommentieren
+//        String dest = getGegenkontoBLZ();
+//        String src  = getKonto().getBLZ();
+//        if (!dest.equals(src))
+//          throw new ApplicationException(i18n.tr("Umbuchungen sind nur zu einem Konto bei Ihrer eigenen Bank möglich"));
       }
     }
     catch (RemoteException e)
@@ -150,6 +152,9 @@ public class UeberweisungImpl extends AbstractBaseUeberweisungImpl implements Ue
 
 /**********************************************************************
  * $Log: UeberweisungImpl.java,v $
+ * Revision 1.45  2011/03/31 16:30:58  willuhn
+ * @N BLZ-Check bei Umbuchungen testhalber mal deaktiviert
+ *
  * Revision 1.44  2010/04/27 11:02:32  willuhn
  * @R Veralteten Verwendungszweck-Code entfernt
  *
