90565f45478dcb07f2bef65649a4ac9856e5ba39
willuhn <willuhn>
2011-04-28 12:15:25 +0000

    @N Wenn beide Umsaetze eine ID haben, muss nur anhand derer verglichen werden

--- src/de/willuhn/jameica/hbci/server/UmsatzImpl.java
+++ src/de/willuhn/jameica/hbci/server/UmsatzImpl.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/server/UmsatzImpl.java,v $
- * $Revision: 1.85 $
- * $Date: 2011/04/28 07:50:07 $
+ * $Revision: 1.86 $
+ * $Date: 2011/04/28 12:15:25 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -281,11 +281,13 @@ public class UmsatzImpl extends AbstractDBObject implements Umsatz
 		{
 			Umsatz other = (Umsatz) o;
 
-			// Wenn die ID uebereinstimmt, sind sie auf jeden Fall gleich. Egal, was die Checksumme sagt
+			// Wenn beide eine ID haben, brauchen wir nur anhand der ID vergleichen
+			// Die Pruefung via Checksumme ist nur noetig, wenn neue Datensaetze
+			// gespeichert werden sollen
 			String id1 = this.getID();
 			String id2 = other.getID();
-			if (id1 != null && id2 != null && id1.equals(id2))
-			  return true;
+			if (id1 != null && id2 != null)
+			  return id1.equals(id2);
 			  
 			return other.getChecksum() == getChecksum();
 		}
@@ -683,7 +685,10 @@ public class UmsatzImpl extends AbstractDBObject implements Umsatz
 
 /**********************************************************************
  * $Log: UmsatzImpl.java,v $
- * Revision 1.85  2011/04/28 07:50:07  willuhn
+ * Revision 1.86  2011/04/28 12:15:25  willuhn
+ * @N Wenn beide Umsaetze eine ID haben, muss nur anhand derer verglichen werden
+ *
+ * Revision 1.85  2011-04-28 07:50:07  willuhn
  * @B BUGZILLA 692
  *
  * Revision 1.84  2011-03-11 15:05:14  willuhn
