e442fc0a33b7127aef4303595b7c09974002284c
willuhn <willuhn>
2011-04-28 07:34:01 +0000

    @C Code-Cleanup

--- src/de/willuhn/jameica/hbci/gui/parts/PassportList.java
+++ src/de/willuhn/jameica/hbci/gui/parts/PassportList.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/parts/PassportList.java,v $
- * $Revision: 1.8 $
- * $Date: 2010/04/14 16:53:01 $
+ * $Revision: 1.9 $
+ * $Date: 2011/04/28 07:34:01 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -19,7 +19,7 @@ import java.util.List;
 
 import de.willuhn.jameica.gui.Action;
 import de.willuhn.jameica.gui.Part;
-import de.willuhn.jameica.gui.parts.CheckedContextMenuItem;
+import de.willuhn.jameica.gui.parts.CheckedSingleContextMenuItem;
 import de.willuhn.jameica.gui.parts.ContextMenu;
 import de.willuhn.jameica.gui.parts.TablePart;
 import de.willuhn.jameica.hbci.HBCI;
@@ -28,7 +28,6 @@ import de.willuhn.jameica.hbci.gui.action.PassportDetail;
 import de.willuhn.jameica.hbci.passport.Passport;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.logging.Logger;
-import de.willuhn.util.ApplicationException;
 import de.willuhn.util.I18N;
 
 /**
@@ -36,7 +35,7 @@ import de.willuhn.util.I18N;
  */
 public class PassportList extends TablePart implements Part
 {
-  private I18N i18n = null;
+  private final static I18N i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
 
   /**
    * @param action
@@ -44,18 +43,10 @@ public class PassportList extends TablePart implements Part
    */
   public PassportList(final Action action) throws RemoteException
   {
-    super(init(), new Action() {
-      public void handleAction(Object context) throws ApplicationException
-      {
-        if (context == null)
-          return;
-        action.handleAction((Passport) context);
-      }
-    });
-    this.i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
+    super(init(),action);
 
     ContextMenu menu = new ContextMenu();
-    menu.addItem(new CheckedContextMenuItem(i18n.tr("Öffnen..."),new PassportDetail(),"document-open.png"));
+    menu.addItem(new CheckedSingleContextMenuItem(i18n.tr("Öffnen..."),new PassportDetail(),"document-open.png"));
     this.setContextMenu(menu);
 
     this.setMulti(false);
@@ -90,31 +81,9 @@ public class PassportList extends TablePart implements Part
 
 /**********************************************************************
  * $Log: PassportList.java,v $
+ * Revision 1.9  2011/04/28 07:34:01  willuhn
+ * @C Code-Cleanup
+ *
  * Revision 1.8  2010/04/14 16:53:01  willuhn
  * @N BUGZILLA 471
- *
- * Revision 1.7  2009/05/07 13:36:57  willuhn
- * @R Hilfsobjekt "PassportObject" entfernt
- * @C Cleanup in PassportInput (insb. der weisse Hintergrund hinter dem "Konfigurieren..."-Button hat gestoert
- *
- * Revision 1.6  2008/12/19 12:16:05  willuhn
- * @N Mehr Icons
- * @C Reihenfolge der Contextmenu-Eintraege vereinheitlicht
- *
- * Revision 1.5  2006/11/24 00:07:09  willuhn
- * @C Konfiguration der Umsatz-Kategorien in View Einstellungen verschoben
- * @N Redesign View Einstellungen
- *
- * Revision 1.4  2005/08/01 23:27:42  web0
- * *** empty log message ***
- *
- * Revision 1.3  2005/07/04 21:57:08  web0
- * @B bug 80
- *
- * Revision 1.2  2005/06/27 15:35:27  web0
- * @B bug 84
- *
- * Revision 1.1  2005/06/21 21:48:24  web0
- * @B bug 80
- *
  **********************************************************************/
