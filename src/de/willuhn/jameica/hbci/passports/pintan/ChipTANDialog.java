8a2a1ced3f8a1f3cb33c379fb19cd13472e0bca1
willuhn <willuhn>
2011-05-09 17:24:46 +0000

    @N ChipTAN-Dialog jetzt in echt

--- src/de/willuhn/jameica/hbci/passports/pintan/ChipTANDialog.java
+++ src/de/willuhn/jameica/hbci/passports/pintan/ChipTANDialog.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/passports/pintan/ChipTANDialog.java,v $
- * $Revision: 1.1 $
- * $Date: 2010/12/08 12:34:57 $
+ * $Revision: 1.2 $
+ * $Date: 2011/05/09 17:24:46 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -19,43 +19,45 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
 import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.gui.Part;
-import de.willuhn.jameica.gui.dialogs.AbstractDialog;
-import de.willuhn.jameica.hbci.HBCI;
+import de.willuhn.jameica.gui.util.Container;
+import de.willuhn.jameica.gui.util.SimpleContainer;
 import de.willuhn.jameica.hbci.passports.pintan.rmi.PinTanConfig;
-import de.willuhn.jameica.system.Application;
+import de.willuhn.jameica.system.Settings;
+import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
-import de.willuhn.util.I18N;
 
 /**
  * Dialog für die ChipTAN-Eingabe.
  */
-public class ChipTANDialog extends AbstractDialog
+public class ChipTANDialog extends TANDialog
 {
-  private final static I18N i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
-  private final static int WINDOW_WIDTH = 550;
-
-  private PinTanConfig config = null;
+  private final static Settings settings = new Settings(ChipTANDialog.class);
+  private String code = null;
   
   /**
    * ct.
    * @param config die PINTAN-Config.
+   * @param code der Flicker-Code.
    * @throws RemoteException
    */
-  public ChipTANDialog(PinTanConfig config) throws RemoteException
+  public ChipTANDialog(PinTanConfig config, String code) throws RemoteException
   {
-    super(ChipTANDialog.POSITION_CENTER);
-    
-    this.config = config;
-		this.setSize(WINDOW_WIDTH,SWT.DEFAULT);
+    super(config);
+    this.code = code;
   }
 
 	/**
@@ -63,20 +65,41 @@ public class ChipTANDialog extends AbstractDialog
    */
   protected void paint(Composite parent) throws Exception
   {
-    FlickerPart flicker = new FlickerPart(null);
+    Container container = new SimpleContainer(parent);
+    container.addHeadline(i18n.tr("Flicker-Grafik"));
+    container.addText(i18n.tr("Ändern Sie die Größe des Fensters so, dass die Flicker-Grafik von Ihrem TAN-Generator gelesen werden kann."),true);
+    
+    FlickerPart flicker = new FlickerPart(this.code);
     flicker.paint(parent);
-    getShell().setMinimumSize(getShell().computeSize(WINDOW_WIDTH,SWT.DEFAULT));
+    super.paint(parent);
+    
+    // Muessen wir ueberschreiben, damit der User den Flicker-Balken passend ziehen kann.
+    getShell().setMinimumSize(100,100);
+    
+    // Geht leider nur mit dem Listener. Alles andere wird ignoriert
+    addShellListener(new ShellAdapter() {
+      public void shellActivated(ShellEvent e)
+      {
+        // Muesste ungefaehr die passende Groesse fuer den TAN-Generator von Reiner SCT sein.
+        final int x = settings.getInt("size.x",269);
+        final int y = settings.getInt("size.y",364);
+        getShell().setSize(x,y);
+      }
+    });
+    
+    
+    parent.addDisposeListener(new DisposeListener() {
+      public void widgetDisposed(DisposeEvent e)
+      {
+        // Wir merken uns die Groesse des Dialogs
+        Point p = getShell().getSize();
+        Logger.info("saving size of flicker dialog: " + p.x + "x" + p.y);
+        settings.setAttribute("size.x",p.x);
+        settings.setAttribute("size.y",p.y);
+      }
+    });
   }
 
-  /**
-   * @see de.willuhn.jameica.gui.dialogs.AbstractDialog#getData()
-   */
-  protected Object getData() throws Exception
-  {
-    return null;
-  }
-  
-  
   /**
    * Implementiert die Flicker-Grafik.
    */
@@ -89,18 +112,18 @@ public class ChipTANDialog extends AbstractDialog
     
     /**
      * ct.
-     * @param code der anzuzeigende Flicker-Code.
+     * @param s der anzuzeigende Flicker-Code.
      * @throws ApplicationException
      */
-    private FlickerPart(String code) throws ApplicationException
+    private FlickerPart(String s) throws ApplicationException
     {
       // 11048714955205123456789F14302C303107 // aus der JS-Demo
       // 002624088715131306389726041,00       // von http://www.onlinebanking-forum.de/phpBB2/viewtopic.php?p=60532
       // 100484652456044356435F14312C30304B   // von bankingportal.sparkasse-freiburg.de
 
-      if (code == null || code.length() == 0)
-        throw new ApplicationException(i18n.tr("Kein Flicker-Code angegeben"));
-
+      FlickerCode fc = new FlickerCode(s);
+      code = fc.getCode();
+      
       // Sync-Identifier vorn dran haengen.
       code = "0FFF" + code;
 
@@ -143,7 +166,6 @@ public class ChipTANDialog extends AbstractDialog
       this.canvas = new Canvas(parent,SWT.BORDER);
       this.canvas.setBackground(GUI.getDisplay().getSystemColor(SWT.COLOR_BLACK));
       this.canvas.setLayoutData(new GridData(GridData.FILL_BOTH));
-      this.canvas.setSize(205,100);
 
       this.canvas.addPaintListener(new PaintListener()
       {
@@ -217,12 +239,18 @@ public class ChipTANDialog extends AbstractDialog
     
     /**
      * ct.
-     * @param code der Code.
-     * @throws Exception wenn der Code falsch ist.
+     * @param s der Code.
+     * @throws ApplicationException wenn der Code falsch ist.
      */
-    private FlickerCode(String code) throws Exception
+    private FlickerCode(String s) throws ApplicationException
     {
-      this.code = code.toUpperCase().replaceAll("[^A-F0-9]","");
+      if (s == null || s.length() == 0)
+        throw new ApplicationException(i18n.tr("Kein Flicker-Code angegeben"));
+
+      this.code = s.toUpperCase().replaceAll("[^A-F0-9]","");
+
+      if (!this.check())
+        throw new ApplicationException(i18n.tr("Flicker-Code ungültig"));
     }
 
     /**
@@ -292,32 +320,40 @@ public class ChipTANDialog extends AbstractDialog
      * Prueft, ob die Checksumme korrekt ist.
      * @return true, wenn die Checksumme korrekt ist.
      */
-    private boolean checksum() throws Exception
+    private boolean check()
     {
-      String test = this.code;
-      
-      /* length check: first byte */
-      int len = test.length() / 2 - 1;
-      test = toHex(len,2) + test.substring(2);
-      
-      /* luhn checksum */
-      String luhndata = getPayload();
-      int luhnsum = 0;
-      for (int i=0; i<luhndata.length(); i+=2)
+      try
       {
-        luhnsum += (1*Integer.parseInt(Character.toString(luhndata.charAt(i)),16)) + quersumme(2*Integer.parseInt(Character.toString(luhndata.charAt(i+1)),16));
+        String test = this.code;
+        
+        /* length check: first byte */
+        int len = test.length() / 2 - 1;
+        test = toHex(len,2) + test.substring(2);
+        
+        /* luhn checksum */
+        String luhndata = getPayload();
+        int luhnsum = 0;
+        for (int i=0; i<luhndata.length(); i+=2)
+        {
+          luhnsum += (1*Integer.parseInt(Character.toString(luhndata.charAt(i)),16)) + quersumme(2*Integer.parseInt(Character.toString(luhndata.charAt(i+1)),16));
+        }
+        luhnsum = (10 - (luhnsum % 10)) % 10;
+        test = test.substring(0,test.length()-2) + toHex(luhnsum,1) + test.substring(test.length()-1);
+        
+        /* xor checksum */
+        int xorsum = 0;
+        for (int i=0; i< test.length()-2; i++)
+        {
+          xorsum ^= Integer.parseInt(Character.toString(test.charAt(i)),16);
+        }
+        test = test.substring(0,test.length()-1) + toHex(xorsum,1);
+        return test.equals(this.code);
       }
-      luhnsum = (10 - (luhnsum % 10)) % 10;
-      test = test.substring(0,test.length()-2) + toHex(luhnsum,1) + test.substring(test.length()-1);
-      
-      /* xor checksum */
-      int xorsum = 0;
-      for (int i=0; i< test.length()-2; i++)
+      catch (Exception e)
       {
-        xorsum ^= Integer.parseInt(Character.toString(test.charAt(i)),16);
+        Logger.error("unable to check flicker code " + this.code,e);
+        return false;
       }
-      test = test.substring(0,test.length()-1) + toHex(xorsum,1);
-      return test.equals(this.code);
     }
   }
 }
@@ -325,7 +361,10 @@ public class ChipTANDialog extends AbstractDialog
 
 /**********************************************************************
  * $Log: ChipTANDialog.java,v $
- * Revision 1.1  2010/12/08 12:34:57  willuhn
+ * Revision 1.2  2011/05/09 17:24:46  willuhn
+ * @N ChipTAN-Dialog jetzt in echt
+ *
+ * Revision 1.1  2010-12-08 12:34:57  willuhn
  * @C Flicker-Code in Dialog verschoben
  *
  **********************************************************************/
