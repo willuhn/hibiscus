78c0ac6de081e42aba62aa2bc35c3a9f80104cbf
willuhn <willuhn>
2011-05-23 14:48:04 +0000

    @R wieder deaktiviert - wegen diesem arroganten Schnoesel hier: http://www.onlinebanking-forum.de/phpBB2/viewtopic.php?p=75512#75512

--- src/de/willuhn/jameica/hbci/gui/views/Settings.java
+++ src/de/willuhn/jameica/hbci/gui/views/Settings.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/views/Settings.java,v $
- * $Revision: 1.53 $
- * $Date: 2011/05/23 12:57:37 $
+ * $Revision: 1.54 $
+ * $Date: 2011/05/23 14:48:04 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -63,7 +63,7 @@ public class Settings extends AbstractView
     TabGroup system = new TabGroup(getTabFolder(),i18n.tr("Grundeinstellungen"));
 		system.addCheckbox(control.getOnlineMode(),i18n.tr("Dauerhafte Internetverbindung, Aufforderung zum Verbinden nicht erforderlich"));
     system.addCheckbox(control.getCachePin(),i18n.tr("PIN-Eingaben für die aktuelle Sitzung zwischenspeichern"));
-    system.addCheckbox(control.getStorePin(),i18n.tr("PIN-Eingaben permanent speichern"));
+    // system.addCheckbox(control.getStorePin(),i18n.tr("PIN-Eingaben permanent speichern"));
     system.addCheckbox(control.getCancelSyncOnError(),i18n.tr("HBCI-Synchronisierung bei Fehler abbrechen"));
     system.addCheckbox(control.getDecimalGrouping(),i18n.tr("Tausender-Trennzeichen bei Geld-Beträgen anzeigen"));
     system.addCheckbox(control.getKontoCheck(),i18n.tr("Kontonummern und Bankleitzahlen mittels Prüfsumme testen"));
@@ -148,7 +148,10 @@ public class Settings extends AbstractView
 
 /**********************************************************************
  * $Log: Settings.java,v $
- * Revision 1.53  2011/05/23 12:57:37  willuhn
+ * Revision 1.54  2011/05/23 14:48:04  willuhn
+ * @R wieder deaktiviert - wegen diesem arroganten Schnoesel hier: http://www.onlinebanking-forum.de/phpBB2/viewtopic.php?p=75512#75512
+ *
+ * Revision 1.53  2011-05-23 12:57:37  willuhn
  * @N optionales Speichern der PINs im Wallet. Ich announce das aber nicht. Ich hab das nur eingebaut, weil mir das Gejammer der User auf den Nerv ging und ich nicht will, dass sich User hier selbst irgendwelche Makros basteln, um die PIN dennoch zu speichern
  *
  * Revision 1.52  2011-05-20 16:22:31  willuhn
