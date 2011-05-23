b70b20cf747368856074c418615aed81e2eeccbb
willuhn <willuhn>
2011-05-23 10:54:26 +0000

    @R BUGZILLA 62 - Liste der bisher gespeicherten TANs loeschen

--- src/de/willuhn/jameica/hbci/Settings.java
+++ src/de/willuhn/jameica/hbci/Settings.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/Settings.java,v $
- * $Revision: 1.63 $
- * $Date: 2010/09/16 09:54:05 $
+ * $Revision: 1.64 $
+ * $Date: 2011/05/23 10:54:26 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -15,6 +15,7 @@ package de.willuhn.jameica.hbci;
 import java.io.File;
 import java.rmi.ConnectException;
 import java.rmi.RemoteException;
+import java.util.Date;
 
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
@@ -331,7 +332,18 @@ public class Settings
   public static Wallet getWallet() throws Exception
   {
 		if (wallet == null)
+		{
       wallet = new Wallet(HBCI.class);
+      
+      // Migration BUGZILLA 62 - Loeschen der Liste der verbrauchten TANs
+      String date = settings.getString("migration.tancache.cleared",null);
+      if (date == null)
+      {
+        Logger.info("migration: removing cache of used tans");
+        wallet.deleteAll("tan.");
+        settings.setAttribute("migration.tancache.cleared",HBCI.LONGDATEFORMAT.format(new Date()));
+      }
+		}
 		return wallet;
   }
   
@@ -404,7 +416,10 @@ public class Settings
 
 /*********************************************************************
  * $Log: Settings.java,v $
- * Revision 1.63  2010/09/16 09:54:05  willuhn
+ * Revision 1.64  2011/05/23 10:54:26  willuhn
+ * @R BUGZILLA 62 - Liste der bisher gespeicherten TANs loeschen
+ *
+ * Revision 1.63  2010-09-16 09:54:05  willuhn
  * @B BUGZILLA #904
  *
  * Revision 1.62  2010/06/17 15:31:28  willuhn
