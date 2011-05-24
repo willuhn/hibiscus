ccf19aea8aa735eb4c6f5efb843c9e1e78d1fc56
willuhn <willuhn>
2011-05-24 10:45:32 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/hbci/gui/DialogFactory.java
+++ src/de/willuhn/jameica/hbci/gui/DialogFactory.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/DialogFactory.java,v $
- * $Revision: 1.38 $
- * $Date: 2011/05/24 09:06:11 $
+ * $Revision: 1.39 $
+ * $Date: 2011/05/24 10:45:32 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -139,7 +139,7 @@ public class DialogFactory
     }
 
     // Wenn wir noch nichts im Cache haben, schauen wir im Wallet - wenn das erlaubt ist
-    if (data == null && Settings.getStorePin())
+    if (data == null && Settings.getStorePin() && (passport instanceof HBCIPassportPinTan))
     {
       String s = (String) Settings.getWallet().get(key);
       if (s != null)
@@ -278,7 +278,10 @@ public class DialogFactory
 
 /**********************************************************************
  * $Log: DialogFactory.java,v $
- * Revision 1.38  2011/05/24 09:06:11  willuhn
+ * Revision 1.39  2011/05/24 10:45:32  willuhn
+ * *** empty log message ***
+ *
+ * Revision 1.38  2011-05-24 09:06:11  willuhn
  * @C Refactoring und Vereinfachung von HBCI-Callbacks
  *
  * Revision 1.37  2011-05-23 12:57:38  willuhn
