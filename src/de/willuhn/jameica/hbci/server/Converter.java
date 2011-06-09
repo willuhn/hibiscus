f2e82a86a6b79a1f3e5270aa900563c495b26618
willuhn <willuhn>
2011-06-09 08:50:10 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/hbci/server/Converter.java
+++ src/de/willuhn/jameica/hbci/server/Converter.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/server/Converter.java,v $
- * $Revision: 1.60 $
- * $Date: 2011/06/09 08:35:18 $
+ * $Revision: 1.61 $
+ * $Date: 2011/06/09 08:50:10 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -87,7 +87,7 @@ public class Converter {
 		umsatz.setDatum(u.bdate);
 		umsatz.setValuta(u.valuta);
 		
-		// TODO: Der GV-Code aus der Umsatzzeile muss mit in Hibiscus uebernommen werden, da er beim MT940-Export wieder gebraucht wird
+		// TODO: (BUGZILLA 1065) Der GV-Code aus der Umsatzzeile muss mit in Hibiscus uebernommen werden, da er beim MT940-Export wieder gebraucht wird
 		// umsatz.setGVCode(u.gvcode);
 
 		////////////////////////////////////////////////////////////////////////////
@@ -354,7 +354,10 @@ public class Converter {
 
 /**********************************************************************
  * $Log: Converter.java,v $
- * Revision 1.60  2011/06/09 08:35:18  willuhn
+ * Revision 1.61  2011/06/09 08:50:10  willuhn
+ * *** empty log message ***
+ *
+ * Revision 1.60  2011-06-09 08:35:18  willuhn
  * *** empty log message ***
  *
  * Revision 1.59  2011-06-07 10:07:50  willuhn
