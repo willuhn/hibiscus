062b0990321c2da1586eaa86cff8e73a68e07ebc
willuhn <willuhn>
2011-10-20 16:18:44 +0000

    @N GUI-Cleanup

--- src/de/willuhn/jameica/hbci/gui/dialogs/HBCIVersionDialog.java
+++ src/de/willuhn/jameica/hbci/gui/dialogs/HBCIVersionDialog.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/dialogs/HBCIVersionDialog.java,v $
- * $Revision: 1.3 $
- * $Date: 2008/07/28 09:31:10 $
+ * $Revision: 1.4 $
+ * $Date: 2011/10/20 16:18:44 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -17,8 +17,9 @@ import org.eclipse.swt.widgets.Composite;
 
 import de.willuhn.jameica.gui.Action;
 import de.willuhn.jameica.gui.dialogs.AbstractDialog;
-import de.willuhn.jameica.gui.util.ButtonArea;
-import de.willuhn.jameica.gui.util.LabelGroup;
+import de.willuhn.jameica.gui.parts.ButtonArea;
+import de.willuhn.jameica.gui.util.Container;
+import de.willuhn.jameica.gui.util.SimpleContainer;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.gui.input.HBCIVersionInput;
 import de.willuhn.jameica.system.Application;
@@ -57,33 +58,37 @@ public class HBCIVersionDialog extends AbstractDialog
    */
   protected void paint(Composite parent) throws Exception
   {
-    LabelGroup group = new LabelGroup(parent,i18n.tr("HBCI-Version"));
+    Container group = new SimpleContainer(parent);
     group.addText(i18n.tr("Bitte wählen Sie die zu verwendende HBCI-Version"),true);
     
     final HBCIVersionInput input = new HBCIVersionInput();
     group.addInput(input);
     
-    ButtonArea buttons = group.createButtonArea(2);
+    ButtonArea buttons = new ButtonArea();
     buttons.addButton(i18n.tr("Übernehmen"), new Action() {
       public void handleAction(Object context) throws ApplicationException
       {
         version = (String) input.getValue();
         close();
       }
-    },null,true);
+    },null,true,"ok.png");
     buttons.addButton(i18n.tr("Abbrechen"), new Action() {
       public void handleAction(Object context) throws ApplicationException
       {
         throw new OperationCanceledException("cancelled while choosing hbci version");
       }
     
-    });
+    },null,false,"process-stop.png");
+    group.addButtonArea(buttons);
   }
 }
 
 
 /*********************************************************************
  * $Log: HBCIVersionDialog.java,v $
+ * Revision 1.4  2011/10/20 16:18:44  willuhn
+ * @N GUI-Cleanup
+ *
  * Revision 1.3  2008/07/28 09:31:10  willuhn
  * @N Abfrage der HBCI-Version via Messaging
  *
