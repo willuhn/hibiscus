f5ee367228a14a7b0aca609f01d9cf0ed0d22c0d
Olaf Willuhn <github@willuhn.de>
2014-02-25 00:07:11 +0100

    Drucken erstmal deaktiviert

--- src/de/willuhn/jameica/hbci/gui/views/SepaSammelLastschriftList.java
+++ src/de/willuhn/jameica/hbci/gui/views/SepaSammelLastschriftList.java
@@ -6,17 +6,12 @@
  **********************************************************************/
 package de.willuhn.jameica.hbci.gui.views;
 
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
-
 import de.willuhn.jameica.gui.AbstractView;
 import de.willuhn.jameica.gui.GUI;
-import de.willuhn.jameica.gui.internal.parts.PanelButtonPrint;
 import de.willuhn.jameica.gui.parts.ButtonArea;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.gui.action.SepaSammelLastschriftImport;
 import de.willuhn.jameica.hbci.gui.controller.SepaSammelLastschriftControl;
-import de.willuhn.jameica.hbci.io.print.PrintSupportSepaSammelLastschrift;
 import de.willuhn.jameica.messaging.StatusBarMessage;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.util.I18N;
@@ -36,24 +31,24 @@ public class SepaSammelLastschriftList extends AbstractView
     SepaSammelLastschriftControl control = new SepaSammelLastschriftControl(this);
     
     final de.willuhn.jameica.hbci.gui.parts.SepaSammelLastschriftList table = control.getListe();
-    final PanelButtonPrint print = new PanelButtonPrint(new PrintSupportSepaSammelLastschrift(table));
-    table.addSelectionListener(new Listener() {
-      public void handleEvent(Event event)
-      {
-        print.setEnabled(table.getSelection() != null);
-      }
-    });
+    // final PanelButtonPrint print = new PanelButtonPrint(new PrintSupportSepaSammelLastschrift(table));
+//    table.addSelectionListener(new Listener() {
+//      public void handleEvent(Event event)
+//      {
+//        print.setEnabled(table.getSelection() != null);
+//      }
+//    });
     
     Application.getMessagingFactory().sendMessage(new StatusBarMessage(i18n.tr("Beta: Unterstützung noch im Test-Stadium, bitte noch nicht produktiv verwenden!"),StatusBarMessage.TYPE_INFO));
 
     GUI.getView().setTitle(i18n.tr("(Beta) Vorhandene SEPA-Sammellastschriften"));
-    GUI.getView().addPanelButton(print);
+//    GUI.getView().addPanelButton(print);
 
     ButtonArea buttons = table.getButtons();
     buttons.addButton(i18n.tr("Importieren..."),new SepaSammelLastschriftImport(),null,false,"document-open.png");
     buttons.addButton(i18n.tr("Neue SEPA-Sammellastschrift"),new de.willuhn.jameica.hbci.gui.action.SepaSammelLastschriftNew(),null,false,"text-x-generic.png");
 
     table.paint(getParent());
-    print.setEnabled(table.getSelection() != null); // einmal initial ausloesen
+//    print.setEnabled(table.getSelection() != null); // einmal initial ausloesen
   }
 }
