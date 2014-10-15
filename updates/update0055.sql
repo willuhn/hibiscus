a6054c035614c5366650436a5f6d0a70faec0d6f
Olaf Willuhn <github@willuhn.de>
2014-10-15 11:59:48 +0200

    Datenbank-Update 0055 konnte auf Postgres nicht ausgeführt werden

--- updates/update0055.sql
+++
@@ -1,5 +0,0 @@
--- ----------------------------------------------------------------------
--- Erweitert die Tabelle "aueberweisung" um die Spalte "umbuchung"
--- ----------------------------------------------------------------------
-
-alter table aueberweisung add umbuchung int(1) NULL;
