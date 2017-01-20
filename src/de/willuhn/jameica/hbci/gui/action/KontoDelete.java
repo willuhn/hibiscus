4949689fc2db52768058736e5bcd6c0908927fae
Olaf Willuhn <github@willuhn.de>
2017-01-20 09:30:02 +0100

    Beim Loeschen eines Kontos im Hinweis-Dialog war nicht von Konto sondern von "Bankverbindung" die Rede. Das irritiert. Siehe http://www.onlinebanking-forum.de/forum/topic.php?t=20930&highlight=&page=last#last_post

--- src/de/willuhn/jameica/hbci/gui/action/KontoDelete.java
+++ src/de/willuhn/jameica/hbci/gui/action/KontoDelete.java
@@ -1,13 +1,8 @@
 /**********************************************************************
- * $Source: /cvsroot/hibiscus/hibiscus/src/de/willuhn/jameica/hbci/gui/action/KontoDelete.java,v $
- * $Revision: 1.4 $
- * $Date: 2011/05/11 10:20:28 $
- * $Author: willuhn $
- * $Locker:  $
- * $State: Exp $
  *
- * Copyright (c) by willuhn.webdesign
+ * Copyright (c) by Olaf Willuhn
  * All rights reserved
+ * GPLv2
  *
  **********************************************************************/
 package de.willuhn.jameica.hbci.gui.action;
@@ -49,8 +44,8 @@ public class KontoDelete implements Action
 				return;
 
 			YesNoDialog d = new YesNoDialog(YesNoDialog.POSITION_CENTER);
-			d.setTitle(i18n.tr("Bankverbindung löschen"));
-			d.setText(i18n.tr("Wollen Sie diese Bankverbindung wirklich löschen?\nHierbei werden auch alle Umsätze, Überweisungen und Daueraufträge des Kontos lokal gelöscht."));
+			d.setTitle(i18n.tr("Konto löschen"));
+			d.setText(i18n.tr("Wollen Sie dieses Konto wirklich löschen?\nHierbei werden auch alle Umsätze, Überweisungen und Daueraufträge des Kontos lokal gelöscht."));
 
 			try {
 				Boolean choice = (Boolean) d.open();
@@ -70,30 +65,13 @@ public class KontoDelete implements Action
 
 			// ok, wir loeschen das Objekt
 			k.delete();
-			GUI.getStatusBar().setSuccessText(i18n.tr("Bankverbindung gelöscht."));
+			GUI.getStatusBar().setSuccessText(i18n.tr("Konto gelöscht."));
 		}
 		catch (RemoteException e)
 		{
-			GUI.getStatusBar().setErrorText(i18n.tr("Fehler beim Löschen der Bankverbindung."));
+			GUI.getStatusBar().setErrorText(i18n.tr("Fehler beim Löschen des Kontos."));
 			Logger.error("unable to delete konto",e);
 		}
   }
 
 }
-
-
-/**********************************************************************
- * $Log: KontoDelete.java,v $
- * Revision 1.4  2011/05/11 10:20:28  willuhn
- * @N OCE fangen
- *
- * Revision 1.3  2004/11/12 18:25:07  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2004/10/25 17:58:56  willuhn
- * @N Haufen Dauerauftrags-Code
- *
- * Revision 1.1  2004/10/20 12:08:18  willuhn
- * @C MVC-Refactoring (new Controllers)
- *
- **********************************************************************/
