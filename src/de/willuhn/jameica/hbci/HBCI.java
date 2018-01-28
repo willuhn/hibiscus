c017a90bb30a6a22d9d4fcd5be6d6bd0a54ee16f
Olaf Willuhn <github@willuhn.de>
2018-01-28 13:45:45 +0100

    Offizielle HBCI-Produktregistrierung von Hibiscus hinterlegt: A44C2953982351617D475443E - siehe http://hbci-zka.de/register/register_faq.htm

--- src/de/willuhn/jameica/hbci/HBCI.java
+++ src/de/willuhn/jameica/hbci/HBCI.java
@@ -215,7 +215,7 @@ public class HBCI extends AbstractPlugin
       this.hbciProps = new Properties();
       
       Version v = getManifest().getVersion(); // client.product.name darf hoechstens 25 Zeichen lang sein
-      this.hbciProps.put("client.product.name","HBCI4Java/Hibiscus");
+      this.hbciProps.put("client.product.name","A44C2953982351617D475443E"); // Das ist die offizielle Produktkennung von Hibiscus - siehe http://hbci-zka.de/register/register_faq.htm
       this.hbciProps.put("client.product.version",v.getMajor() + "." + v.getMinor()); // Maximal 5 Zeichen
       
       //////////////////////////////////
