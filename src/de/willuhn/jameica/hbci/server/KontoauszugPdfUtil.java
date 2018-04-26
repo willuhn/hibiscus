757ea8e5ec4f66d063f1addb83294ecc00f94a67
Olaf Willuhn <github@willuhn.de>
2018-04-26 13:13:00 +0200

    Abrufdatum als Fallback statt Erstellungsdatum

--- src/de/willuhn/jameica/hbci/server/KontoauszugPdfUtil.java
+++ src/de/willuhn/jameica/hbci/server/KontoauszugPdfUtil.java
@@ -388,6 +388,8 @@ public class KontoauszugPdfUtil
       Calendar cal = Calendar.getInstance();
       if (ka != null && ka.getErstellungsdatum() != null)
         cal.setTime(ka.getErstellungsdatum());
+      else
+        cal.setTime(ka.getAusfuehrungsdatum());
       
       Integer i = ka != null && ka.getJahr() != null ? ka.getJahr() : null;
       ctx.put("jahr",i != null ? i.toString() : Integer.toString(cal.get(Calendar.YEAR)));
