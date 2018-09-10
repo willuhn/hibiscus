dd1319991f39b2a6d2e9a25b450fce0aa3170c2e
Olaf Willuhn <github@willuhn.de>
2018-09-10 14:58:32 +0200

    Da fehlten die Konten

--- src/de/willuhn/jameica/hbci/gui/boxes/Konten.java
+++ src/de/willuhn/jameica/hbci/gui/boxes/Konten.java
@@ -20,6 +20,7 @@ import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.Settings;
 import de.willuhn.jameica.hbci.gui.action.KontoNew;
 import de.willuhn.jameica.hbci.gui.parts.KontoList;
+import de.willuhn.jameica.hbci.server.KontoUtil;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.util.I18N;
 
@@ -67,7 +68,7 @@ public class Konten extends AbstractBox implements Box
    */
   public void paint(Composite parent) throws RemoteException
   {
-    KontoList l = new KontoList(null,new KontoNew());
+    KontoList l = new KontoList(KontoUtil.getKonten(null),new KontoNew());
     l.setShowFilter(false);
     l.paint(parent);
   }
