f2326e3978dbf084cbe8a29c995843e04f3d2cf7
Olaf Willuhn <github@willuhn.de>
2019-02-19 18:22:08 +0100

    Code 3290 ebenfalls tolerieren (keine Dauerauftraege beim Abruf)

--- src/de/willuhn/jameica/hbci/server/hbci/AbstractHBCIJob.java
+++ src/de/willuhn/jameica/hbci/server/hbci/AbstractHBCIJob.java
@@ -244,11 +244,12 @@ public abstract class AbstractHBCIJob
       // Sonderfall: Wenn keine Umsaetze vorliegen, senden manche Banken nicht "0020 - Es sind keine Umsätze vorhanden." sondern
       // "3010 - Für Konto <nr> liegen keine Daten vor.". Siehe https://homebanking-hilfe.de/forum/topic.php?t=22461&page=fst_unread
       // Oder "3010 - Umsatzabfrage: Keine Einträge vorhanden."
+      // 3290 senden manche Banken beim Abruf der Dauerauftraege, wenn keine vorhanden waren
       HBCIRetVal[] warnings = status.getWarnings();
       if (warnings != null && warnings.length == 1 && warnings[0].code != null && warnings[0].text != null)
       {
         Logger.info("institute did not sent 0xxx success code - only " + warnings[0].toString());
-        if (warnings[0].code.equals("3010"))
+        if (warnings[0].code.equals("3010") || warnings[0].code.equals("3290"))
         {
           executed = true;
           haveJobStatus = true;
