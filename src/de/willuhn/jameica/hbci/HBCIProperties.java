b36f2fe4d5d224f43baaa3cf85ad9c7a5a2a01a1
Olaf Willuhn <github@willuhn.de>
2019-03-25 11:06:40 +0100

    HASPA: Bei Anlage eines neuen Bankzugangs automatisch die neue URL vorschlagen, wenn das Systemdatum der Umstellungstag 20.04.2019 ist

--- src/de/willuhn/jameica/hbci/HBCIProperties.java
+++ src/de/willuhn/jameica/hbci/HBCIProperties.java
@@ -10,6 +10,7 @@
 package de.willuhn.jameica.hbci;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -17,7 +18,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
+import org.kapott.hbci.manager.BankInfo;
 import org.kapott.hbci.manager.HBCIUtils;
 import org.kapott.hbci.passport.HBCIPassport;
 import org.kapott.hbci.structures.Konto;
@@ -420,6 +423,38 @@ public class HBCIProperties
     // Namen haben: "Landesbank Baden-Württemberg/Baden-Württembergische Bank"
     // Das verzerrt sonst die Layouts an einigen Stellen
     return StringUtils.abbreviateMiddle(bank.getBezeichnung(),"...",38);
+  }
+  
+  /**
+   * Liefert die Bankdaten fuer die Bank.
+   * @param blz die BLZ.
+   * @return die Bankdaten. NULL, wenn sie nicht gefunden wurden.
+   */
+  public static BankInfo getBankInfo(String blz)
+  {
+    BankInfo info = HBCIUtils.getBankInfo(blz);
+    
+    if (info == null)
+      return null;
+    
+    // MIGRATION HASPA: Die Haspa stellt am 20.04.2019 von ihrem eigenen System auf Finanz IT um.
+    // Auch wenn die blz.properties in HBCI4Java die neue URL erst ab diesem Tag enthalten wird,
+    // wollen wir trotzdem schon vorher eine Hibiscus-Version ausliefern, die abhaengig vom aktuellen
+    // Datum die korrekte URL liefert.
+    if (ObjectUtils.equals(info.getBlz(),"20050550"))
+    {
+      // Datum checken
+      Calendar cal = Calendar.getInstance();
+      cal.set(Calendar.YEAR,2019);
+      cal.set(Calendar.MONTH,Calendar.APRIL);
+      cal.set(Calendar.DAY_OF_MONTH,20);
+      final Date date = DateUtil.startOfDay(cal.getTime());
+      final Date now = new Date();
+      if (now.after(date))
+        info.setPinTanAddress("https://banking-hh7.s-fints-pt-hh.de/fints30");
+    }
+    
+    return info;
   }
 
   /**
