bd6f2d7ae918c2b2427b48bbe809359f069ad39b
Olaf Willuhn <github@willuhn.de>
2019-04-16 10:43:58 +0200

    TICKET #186 - Import von mehreren CAMT-Dateien in einer ZIP-Datei

--- src/de/willuhn/jameica/hbci/io/CamtUmsatzImporter.java
+++ src/de/willuhn/jameica/hbci/io/CamtUmsatzImporter.java
@@ -16,6 +16,8 @@ import java.io.InputStream;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.kapott.hbci.GV.parsers.ISEPAParser;
 import org.kapott.hbci.GV.parsers.SEPAParserFactory;
@@ -28,7 +30,7 @@ import de.willuhn.io.IOUtil;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.gui.dialogs.KontoAuswahlDialog;
 import de.willuhn.jameica.hbci.messaging.ImportMessage;
-import de.willuhn.jameica.hbci.rmi.Protokoll;
+import de.willuhn.jameica.hbci.rmi.Konto;
 import de.willuhn.jameica.hbci.rmi.Umsatz;
 import de.willuhn.jameica.hbci.server.Converter;
 import de.willuhn.jameica.system.Application;
@@ -56,8 +58,10 @@ public class CamtUmsatzImporter implements Importer
     if (is == null)
       throw new ApplicationException(i18n.tr("Keine zu importierende Datei ausgewählt"));
     
-    if (format == null)
+    if (format == null || !(format instanceof MyIOFormat))
       throw new ApplicationException(i18n.tr("Kein Datei-Format ausgewählt"));
+    
+    final MyIOFormat myFormat = (MyIOFormat) format;
     
     try
     {
@@ -77,80 +81,45 @@ public class CamtUmsatzImporter implements Importer
       if (monitor != null)
         monitor.setStatusText(i18n.tr("Lese Datei ein"));
 
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      IOUtil.copy(is,bos);
+      Stats stats = new Stats();
       
-      SepaVersion version = SepaVersion.autodetect(new ByteArrayInputStream(bos.toByteArray()));
-      if (version == null)
-        throw new ApplicationException(i18n.tr("SEPA-Version der XML-Datei nicht ermittelbar"));
-
-      SepaVersion.Type type = version.getType();
-      if (type == null || type != SepaVersion.Type.CAMT_052)
-        throw new ApplicationException(i18n.tr("Keine gültige CAMT-Datei"));
-
-      monitor.log(i18n.tr("SEPA-Version: {0}",version.getURN()));
-
-      List<BTag> tage = new ArrayList<BTag>();
-      ISEPAParser<List<BTag>> parser = SEPAParserFactory.get(version);
-      parser.parse(new ByteArrayInputStream(bos.toByteArray()),tage);
-      
-      List<UmsLine> lines = new ArrayList<UmsLine>();
-      for (BTag tag:tage)
+      if (myFormat.isZip())
       {
-        lines.addAll(tag.lines);
-      }
-
-      if (lines.size() == 0)
-      {
-        konto.addToProtokoll(i18n.tr("Keine Umsätze importiert"),Protokoll.TYP_ERROR);
-        return;
-      }
-      
-      double factor = 100d / (double) lines.size();
-
-      int created = 0;
-      int error   = 0;
-
-      for (int i=0;i<lines.size();++i)
-      {
-        if (monitor != null)
+        Logger.info("reading zip file");
+        // Wir kriegen aus dem ZIP-Inputstream nicht raus, wieviele Dateien enthalten sind sondern koennen nur drueber iterieren.
+        // Daher rechnen wir fuer den Fortschrittsbalken mal pauschal mit 22 Dateien (pro Buchungstag aka Wochentag eines Monats eine Datei)
+        ZipInputStream zis = new ZipInputStream(is);
+        ZipEntry ze = null;
+        double fc = 22d;
+        int no = 1;
+        while ((ze = zis.getNextEntry()) != null)
         {
-          monitor.log(i18n.tr("Umsatz {0}", "" + (i+1)));
-          monitor.setPercentComplete((int)((i+1) * factor));
-        }
-
-        try
-        {
-          if (t != null && t.isInterrupted())
-            throw new OperationCanceledException();
-
-          final Umsatz umsatz = Converter.HBCIUmsatz2HibiscusUmsatz((GVRKUms.UmsLine)lines.get(i));
-          umsatz.setKonto(konto); // muessen wir noch machen, weil der Converter das Konto nicht kennt
-          umsatz.store();
-          created++;
-          try
-          {
-            Application.getMessagingFactory().sendMessage(new ImportMessage(umsatz));
-          }
-          catch (Exception ex)
-          {
-            Logger.error("error while sending import message",ex);
-          }
-        }
-        catch (ApplicationException ae)
-        {
-          monitor.log("  " + ae.getMessage());
-          error++;
-        }
-        catch (Exception e)
-        {
-          Logger.error("unable to import line",e);
-          monitor.log("  " + i18n.tr("Fehler beim Import des Datensatzes: {0}",e.getMessage()));
-          error++;
+          if (ze.isDirectory())
+            continue;
+          
+          String name = ze.getName();
+          if (name == null)
+            continue;
+          
+          if (!name.toLowerCase().endsWith(".xml"))
+            continue;
+          
+          Logger.info("  reading " + name);
+          
+          int chunk = (int) (100 / fc);
+          Stats s = doImport(chunk,no,zis,konto,monitor,t);
+          stats.created += s.created;
+          stats.error += s.error;
+          no++;
         }
       }
-      monitor.setStatusText(i18n.tr("{0} Umsätze erfolgreich importiert, {1} fehlerhafte übersprungen", new String[]{""+created,""+error}));
-      monitor.addPercentComplete(1);
+      else
+      {
+        Logger.info("reading xml file");
+        stats = doImport(100,1,is,konto,monitor,t);
+      }
+      
+      monitor.setStatusText(i18n.tr("{0} Umsätze erfolgreich importiert, {1} fehlerhafte übersprungen", new String[]{Integer.toString(stats.created),Integer.toString(stats.error)}));
     }
     catch (OperationCanceledException oce)
     {
@@ -171,13 +140,113 @@ public class CamtUmsatzImporter implements Importer
       IOUtil.close(is);
     }
   }
+  
+  /**
+   * Fuehrt den eigentlichen Import einer Datei durch.
+   * @param chunk die prozentuale Groesse des Chunks, den die Datei im Gesamt-Fortschritt einnehmen darf.
+   * @param no die Dateinummer.
+   * @param is der Stream mit der XML-Datei.
+   * @param konto das Konto.
+   * @param monitor der Progress-Monitor.
+   * @param t der Background-Task.
+   * @return die Import-Statistik.
+   * @throws Exception
+   */
+  private Stats doImport(int chunk, int no, InputStream is, Konto konto, ProgressMonitor monitor, BackgroundTask t) throws Exception
+  {
+    Stats stats = new Stats();
+    
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    IOUtil.copy(is,bos);
+    
+    SepaVersion version = SepaVersion.autodetect(new ByteArrayInputStream(bos.toByteArray()));
+    if (version == null)
+      throw new ApplicationException(i18n.tr("SEPA-Version der XML-Datei nicht ermittelbar"));
 
+    SepaVersion.Type type = version.getType();
+    if (type == null || type != SepaVersion.Type.CAMT_052)
+      throw new ApplicationException(i18n.tr("Keine gültige CAMT-Datei"));
+
+    if (monitor != null)
+      monitor.log(i18n.tr("Datei {0}, SEPA-Version: {1}",Integer.toString(no),version.getURN()));
+
+    List<BTag> tage = new ArrayList<BTag>();
+    ISEPAParser<List<BTag>> parser = SEPAParserFactory.get(version);
+    parser.parse(new ByteArrayInputStream(bos.toByteArray()),tage);
+    
+    List<UmsLine> lines = new ArrayList<UmsLine>();
+    for (BTag tag:tage)
+    {
+      lines.addAll(tag.lines);
+    }
+
+    double factor = chunk / (double) lines.size();
+
+    for (int i=0;i<lines.size();++i)
+    {
+      if (monitor != null)
+      {
+        int add = (int)((i+1) * factor);
+        monitor.log(i18n.tr("Umsatz {0}", Integer.toString(i+1)));
+        
+        int offset = 0;
+        if (chunk < 100)
+          offset = chunk * no;
+        monitor.setPercentComplete(offset + add);
+      }
+
+      try
+      {
+        if (t != null && t.isInterrupted())
+          throw new OperationCanceledException();
+
+        final Umsatz umsatz = Converter.HBCIUmsatz2HibiscusUmsatz((GVRKUms.UmsLine)lines.get(i));
+        umsatz.setKonto(konto); // muessen wir noch machen, weil der Converter das Konto nicht kennt
+        umsatz.store();
+        stats.created++;
+        try
+        {
+          Application.getMessagingFactory().sendMessage(new ImportMessage(umsatz));
+        }
+        catch (Exception ex)
+        {
+          Logger.error("error while sending import message",ex);
+        }
+      }
+      catch (ApplicationException ae)
+      {
+        if (monitor != null)
+          monitor.log("  " + ae.getMessage());
+        stats.error++;
+      }
+      catch (Exception e)
+      {
+        Logger.error("unable to import line",e);
+        
+        if (monitor != null)
+          monitor.log("  " + i18n.tr("Fehler beim Import des Datensatzes: {0}",e.getMessage()));
+        stats.error++;
+      }
+    }
+    
+    return stats;
+  }
+  
+  /**
+   * Kapselt die Counter mit den erstellten und fehlerhaften Buchungen.
+   */
+  private class Stats
+  {
+    private int created = 0;
+    private int error = 0;
+  }
+  
   /**
    * @see de.willuhn.jameica.hbci.io.IO#getName()
    */
   public String getName()
   {
-    return i18n.tr("SEPA CAMT-Format (XML)");
+    return i18n.tr("SEPA CAMT-Format");
   }
 
   /**
@@ -188,10 +257,19 @@ public class CamtUmsatzImporter implements Importer
     if (!Umsatz.class.equals(objectType))
       return null; // Wir bieten uns nur fuer Umsaetze an
     
-    IOFormat f = new IOFormat() {
+    IOFormat fXml = new MyIOFormat() {
       public String getName()
       {
-        return CamtUmsatzImporter.this.getName();
+        return CamtUmsatzImporter.this.getName() + " (XML)";
+      }
+      
+      /**
+       * @see de.willuhn.jameica.hbci.io.CamtUmsatzImporter.MyIOFormat#isZip()
+       */
+      @Override
+      public boolean isZip()
+      {
+        return false;
       }
 
       /**
@@ -202,6 +280,42 @@ public class CamtUmsatzImporter implements Importer
         return new String[] {"*.xml"};
       }
     };
-    return new IOFormat[] { f };
+    IOFormat fZip = new MyIOFormat() {
+      public String getName()
+      {
+        return CamtUmsatzImporter.this.getName() + " (ZIP)";
+      }
+      
+      /**
+       * @see de.willuhn.jameica.hbci.io.CamtUmsatzImporter.MyIOFormat#isZip()
+       */
+      @Override
+      public boolean isZip()
+      {
+        return true;
+      }
+
+      /**
+       * @see de.willuhn.jameica.hbci.io.IOFormat#getFileExtensions()
+       */
+      public String[] getFileExtensions()
+      {
+        return new String[] {"*.zip"};
+      }
+    };
+    return new IOFormat[] { fXml,fZip };
   }
+  
+  /**
+   * Fuegt einen Marker hinzu, um zu erkennen, ob es eine ZIP-Datei ist.
+   */
+  private interface MyIOFormat extends IOFormat
+  {
+    /**
+     * Liefert true, wenn es eine ZIP-Datei ist.
+     * @return true, wenn es eine ZIP-Datei ist.
+     */
+    public boolean isZip();
+  }
+  
 }
