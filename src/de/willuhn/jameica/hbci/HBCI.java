b764f12a3350fd5b484cb7f3ea43c9cffe703e21
Olaf Willuhn <github@willuhn.de>
2019-09-06 11:59:28 +0200

    Update auf HBCI4Java 3.1.4

--- src/de/willuhn/jameica/hbci/HBCI.java
+++ src/de/willuhn/jameica/hbci/HBCI.java
@@ -75,7 +75,7 @@ public class HBCI extends AbstractPlugin
    */
   public final static HashMap LOGMAPPING = new HashMap();
   
-  private final static String HBCI4JAVA_VERSION = "3.1.4-SNAPSHOT";
+  private final static String HBCI4JAVA_VERSION = "3.1.4";
 
   private HBCICallback callback = null;
   private Properties hbciProps  = null;
