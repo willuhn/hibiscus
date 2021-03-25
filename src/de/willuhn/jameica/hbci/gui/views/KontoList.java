99c014180037e2767edfc3556141281894018a71
Olaf Willuhn <github@willuhn.de>
2021-03-25 08:50:46 +0100

    Beim PhotoTAN-Dialog auch die Fenstergröße mit speichern

--- src/de/willuhn/jameica/hbci/gui/views/KontoList.java
+++ src/de/willuhn/jameica/hbci/gui/views/KontoList.java
@@ -9,10 +9,14 @@
  **********************************************************************/
 package de.willuhn.jameica.hbci.gui.views;
 
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 import de.willuhn.jameica.gui.AbstractView;
 import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.gui.controller.KontoControl;
+import de.willuhn.jameica.hbci.passports.pintan.PhotoTANDialog;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.util.I18N;
 
@@ -29,6 +33,20 @@ public class KontoList extends AbstractView
    */
   public void bind() throws Exception
   {
+    FileInputStream fis = new FileInputStream("/work/willuhn/git/hbci4java/src/test/resources/org/kapott/hbci4java/secmech/TestMatrixCode-001.txt");
+    InputStreamReader reader = new InputStreamReader(fis,"UTF-8");
+    
+    StringBuilder sb = new StringBuilder();
+    char[] buf = new char[1024];
+    int read = 0;
+    while ((read = reader.read(buf)) != -1)
+    {
+      sb.append(buf,0,read);
+    }
+    fis.close();
+    PhotoTANDialog d = new PhotoTANDialog(null,sb.toString());
+    d.setText("Smart-TAN photo\nChallenge\n\n1. Stecken Sie Ihre Chipkarte in den SmatrtTAN-Leser und drücken Sie Scan.\n2.Halten Sie den SmartTAN-Leser so vor die ashdf aldfhd,.fjg skdfljhglksehrgl kjsdfljksdlfgh sldkjfgh sdlkjfh lskdjfhgl ksdjhg lkjsdhgfl ksjdh flgkj sdhlk ghsdlkfjh lksdjh flk jsdlkjfgh lksdh fglksjdhflkg sldköfh dsö fsdölkfjg ösdfjksdfjkg löksdfjgpw85 ösfojgh ösdfhög jfhög flköhg ösdfkjh ödfkjgh ökdg hsdöfgh ödshgalsdf lauigh ldaioqzwero  347ahsdvjklhgasdkufg aslhgb asdgvf asdflg\nasdhasljdgfasgfuzqwgerfg jhasdgfegzrfl hadf\naskljdf lasdfhgasdkjhfg ljsdhfhj sdfjdsljj sd\naskdjf asdjfg fsdjasdjfhgljasdf lj adlfgh ldf");
+    d.open();
 
 		final KontoControl control = new KontoControl(this);
 		GUI.getView().setTitle(i18n.tr("Vorhandene Konten"));
