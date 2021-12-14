003d635bc472da6501751d43572a0e9e63181eb8
Olaf Willuhn <github@willuhn.de>
2021-12-14 13:20:13 +0100

    Revert "Refactoring im Exportdialog (#98)" (#100)
    This reverts commit b406c76607c7cc94a09f378eac42fe683a46df2c.

--- src/de/willuhn/jameica/hbci/io/Importer.java
+++ src/de/willuhn/jameica/hbci/io/Importer.java
@@ -19,20 +19,12 @@ import de.willuhn.util.ProgressMonitor;
 
 /**
  * Basis-Interface aller Importer.
- * <p>
- * Alle Klassen, die dieses Interface implementieren, werden automatisch
- * von Hibiscus erkannt und dem Benutzer als Import-Möglichkeit angeboten,
- * wenn sie einen parameterlosen Konstruktor mit dem Modifier "public"
- * besitzen (Java-Bean-Konvention).
- *
- * @see de.willuhn.jameica.hbci.io.IORegistry
  */
 public interface Importer extends IO
 {
 
   /**
    * Importiert Daten aus dem InputStream.
-   *
    * @param context Context, der dem Importer hilft, den Zusammenhang zu erkennen,
    * in dem er aufgerufen wurde. Das kann zum Beispiel ein Konto sein.
    * @param format das vom User ausgewaehlte Import-Format.
@@ -41,7 +33,6 @@ public interface Importer extends IO
    * Bearbeitungszustand ausgeben kann.
    * Der Importer muss den Import-Stream selbst schliessen!
    * @param t der {@link BackgroundTask}
-   *
    * @throws RemoteException
    * @throws ApplicationException 
    */
