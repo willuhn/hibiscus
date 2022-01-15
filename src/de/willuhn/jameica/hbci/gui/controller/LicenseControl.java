87c7c38d546d0b38e3c30b575155c5cd78d4e60f
Olaf Willuhn <github@willuhn.de>
2022-01-15 09:01:46 +0100

    Zu PR #78

--- src/de/willuhn/jameica/hbci/gui/controller/LicenseControl.java
+++ src/de/willuhn/jameica/hbci/gui/controller/LicenseControl.java
@@ -79,17 +79,16 @@ public class LicenseControl extends AbstractControl {
 
     FileFinder finder = new FileFinder(new File(path + "/lib"));
     finder.matches(".*?info\\.xml$");
-    File[] infos = finder.findRecursive();
-    for (int i=0;i<infos.length;++i)
+    for (File file:finder.findRecursive())
     {
-      if (!infos[i].isFile() || !infos[i].canRead())
+      if (!file.isFile() || !file.canRead())
       {
-        Logger.warn("unable to read " + infos[i] + ", skipping");
+        Logger.warn("unable to read " + file + ", skipping");
         continue;
       }
 
       try {
-        InfoReader ir = new InfoReader(new FileInputStream(infos[i]));
+        InfoReader ir = new InfoReader(new FileInputStream(file));
         buffer.append("<p>");
         buffer.append("<b>" + ir.getName() + "</b>");
         buffer.append("<br/>" + ir.getDescription());
@@ -99,7 +98,7 @@ public class LicenseControl extends AbstractControl {
       }
       catch (Exception e)
       {
-        Logger.error("unable to parse " + infos[0],e);
+        Logger.error("unable to parse " + file,e);
       }
     }
     buffer.append("</form>");
@@ -108,59 +107,3 @@ public class LicenseControl extends AbstractControl {
     return libList;
   }
 }
-
-
-/**********************************************************************
- * $Log: LicenseControl.java,v $
- * Revision 1.16  2011/06/08 09:16:51  willuhn
- * *** empty log message ***
- *
- * Revision 1.15  2011-04-26 12:15:51  willuhn
- * @B Potentielle Bugs gemaess Code-Checker
- *
- * Revision 1.14  2009/03/10 23:51:31  willuhn
- * @C PluginResources#getPath als deprecated markiert - stattdessen sollte jetzt Manifest#getPluginDir() verwendet werden
- *
- * Revision 1.13  2006/06/30 13:51:54  willuhn
- * @N Pluginloader Redesign in HEAD uebernommen
- *
- * Revision 1.12  2005/03/09 01:07:02  web0
- * @D javadoc fixes
- *
- * Revision 1.11  2004/11/12 18:25:07  willuhn
- * *** empty log message ***
- *
- * Revision 1.10  2004/10/20 12:08:18  willuhn
- * @C MVC-Refactoring (new Controllers)
- *
- * Revision 1.9  2004/10/11 22:41:25  willuhn
- * *** empty log message ***
- *
- * Revision 1.8  2004/10/08 13:37:47  willuhn
- * *** empty log message ***
- *
- * Revision 1.7  2004/10/08 00:19:08  willuhn
- * *** empty log message ***
- *
- * Revision 1.6  2004/07/25 17:15:05  willuhn
- * @C PluginLoader is no longer static
- *
- * Revision 1.5  2004/07/21 23:54:30  willuhn
- * *** empty log message ***
- *
- * Revision 1.4  2004/07/09 00:04:40  willuhn
- * @C Redesign
- *
- * Revision 1.3  2004/06/30 20:58:28  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2004/06/08 22:28:58  willuhn
- * *** empty log message ***
- *
- * Revision 1.1  2004/04/26 22:57:32  willuhn
- * @N License informations
- *
- * Revision 1.1  2004/04/26 22:42:18  willuhn
- * @N added InfoReader
- *
- **********************************************************************/
