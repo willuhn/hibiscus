32b0776a510af816e793862b596a1ea1aa939c0d
Mihai Moldovan <ionic@ionic.de>
2022-04-22 15:35:41 +0200

    gui: fix plural typo in settings. (#132)

--- src/de/willuhn/jameica/hbci/gui/views/Settings.java
+++ src/de/willuhn/jameica/hbci/gui/views/Settings.java
@@ -72,7 +72,7 @@ public class Settings extends AbstractView implements Extendable
     ui.addLabelPair(i18n.tr("Textfarbe von Habenbuchungen"),control.getBuchungHabenForeground());
     ui.addHeadline(i18n.tr("Formatierung"));
     ui.addCheckbox(control.getDecimalGrouping(),i18n.tr("Tausender-Trennzeichen bei Geld-Beträgen anzeigen"));
-    ui.addCheckbox(control.getBoldValues(),i18n.tr("Geld-Beträgen fett gedruckt anzeigen"));
+    ui.addCheckbox(control.getBoldValues(),i18n.tr("Geld-Beträge fett gedruckt anzeigen"));
 
     // Umsatz-Kategorien
     TabGroup umsatztypes = new TabGroup(getTabFolder(),i18n.tr("Umsatz-Kategorien"));
