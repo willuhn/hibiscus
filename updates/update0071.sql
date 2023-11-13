516ab05e96d3fffa6e666b643d37ca52a3f37724
tlei <github@tleine.de>
2023-11-13 19:00:16 +0100

    Erweiterung der Datenbank um Creditor-ID
    Tabelle Umsatz wird um Spalte creditorid mit Länge 35 erweitert.
    Update der vorhandenen Tabellen in update0071.sql

---
+++ updates/update0071.sql
@@ -0,0 +1,5 @@
+-- ----------------------------------------------------------------------
+-- Erweitert die Tabelle "umsatz" um eine Spalte "creditorid"
+-- ----------------------------------------------------------------------
+
+ALTER TABLE umsatz ADD creditorid varchar(35);
