c8b0c2d4caf3d18972a6149f570bb2ab5f2a0153
tlei <github@tleine.de>
2023-11-16 08:44:01 +0100

    DB-Version richtig setzen

--- sql/h2-create.sql
+++ sql/h2-create.sql
@@ -455,6 +455,6 @@ CREATE INDEX idx_umsatz_datum ON umsatz(datum);
 CREATE INDEX idx_umsatz_valuta ON umsatz(valuta);
 CREATE INDEX idx_umsatz_flags ON umsatz(flags);
   
-INSERT INTO version (name,version) values ('db',70);
+INSERT INTO version (name,version) values ('db',71);
   
 COMMIT;
