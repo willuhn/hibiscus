b0f9714ca59adfd47be485b4973747e6cb4d95ae
Olaf Willuhn <github@willuhn.de>
2024-11-25 11:12:38 +0100

    Update auf HBCI4Java 3.1.85 - https://github.com/hbci4j/hbci4java/releases/tag/hbci4j-core-3.1.85 - siehe https://homebanking-hilfe.de/forum/topic.php?p=174231#real174231

--- src/de/willuhn/jameica/hbci/HBCI.java
+++ src/de/willuhn/jameica/hbci/HBCI.java
@@ -86,7 +86,7 @@ public class HBCI extends AbstractPlugin
    */
   public final static HashMap LOGMAPPING = new HashMap();
   
-  private final static String HBCI4JAVA_VERSION = "3.1.84";
+  private final static String HBCI4JAVA_VERSION = "3.1.85";
 
   private HBCICallback callback = null;
   private Properties hbciProps  = null;
