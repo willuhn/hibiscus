a12d3902f091dc5e5c151b0722b71fc0967aa484
Olaf Willuhn <github@willuhn.de>
2025-09-01 11:00:52 +0200

    Update auf HBCI4Java 3.1.91 - https://github.com/hbci4j/hbci4java/releases/tag/hbci4j-core-3.1.91 - mit neuen Sparda-URLs (Sparda-Bank West)

--- src/de/willuhn/jameica/hbci/HBCI.java
+++ src/de/willuhn/jameica/hbci/HBCI.java
@@ -86,7 +86,7 @@ public class HBCI extends AbstractPlugin
    */
   public final static HashMap LOGMAPPING = new HashMap();
   
-  private final static String HBCI4JAVA_VERSION = "3.1.90";
+  private final static String HBCI4JAVA_VERSION = "3.1.91";
 
   private HBCICallback callback = null;
   private Properties hbciProps  = null;
