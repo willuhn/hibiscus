65f4b831276465938b24390dd919ba2b3ac1a1aa
Olaf Willuhn <github@willuhn.de>
2025-09-05 14:30:03 +0200

    Update auf HBCI4Java 4.0.1 - https://github.com/hbci4j/hbci4java/releases/tag/hbci4j-core-4.0.1

--- src/de/willuhn/jameica/hbci/HBCI.java
+++ src/de/willuhn/jameica/hbci/HBCI.java
@@ -86,7 +86,7 @@ public class HBCI extends AbstractPlugin
    */
   public final static HashMap LOGMAPPING = new HashMap();
   
-  private final static String HBCI4JAVA_VERSION = "3.1.91";
+  private final static String HBCI4JAVA_VERSION = "4.0.1";
 
   private HBCICallback callback = null;
   private Properties hbciProps  = null;
