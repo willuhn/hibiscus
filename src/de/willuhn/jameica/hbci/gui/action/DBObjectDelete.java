d283cb29aed3b96332b382a4f092c071fdd3845d
MichaelOF <MichaelOF@rocketmail.com>
2026-02-16 01:33:46 +0100

    Umsatzkategorien: Sicherheitsabfrage beim Löschen verbessert
    Die Löschabfrage für Umsatzkategorien wurde minimalinvasiv erweitert.
    
    Bei Einzel- und Mehrfachauswahl erscheint eine spezielle Warnung,
    sobald mindestens eine betroffene Kategorie Umsätze zugeordnet hat.
    Dabei werden auch Unterkategorien berücksichtigt.
    
    Das fachliche Löschverhalten bleibt unverändert.
    Keine DB-Änderung, keine Matching-Änderung, keine Encoding-Migration.

--- src/de/willuhn/jameica/hbci/gui/action/DBObjectDelete.java
+++ src/de/willuhn/jameica/hbci/gui/action/DBObjectDelete.java
@@ -11,14 +11,23 @@ package de.willuhn.jameica.hbci.gui.action;
 
 import java.rmi.RemoteException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import de.willuhn.datasource.rmi.DBObject;
 import de.willuhn.datasource.rmi.DBObjectNode;
+import de.willuhn.datasource.rmi.DBIterator;
 import de.willuhn.jameica.gui.Action;
 import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.gui.dialogs.YesNoDialog;
 import de.willuhn.jameica.hbci.HBCI;
+import de.willuhn.jameica.hbci.Settings;
 import de.willuhn.jameica.hbci.messaging.ObjectDeletedMessage;
+import de.willuhn.jameica.hbci.rmi.UmsatzTyp;
 import de.willuhn.jameica.messaging.StatusBarMessage;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.jameica.system.BackgroundTask;
@@ -59,17 +68,45 @@ public class DBObjectDelete implements Action
     }
 
     boolean array = (context instanceof DBObject[]);
+    DBObject[] objects = array ? (DBObject[]) context : new DBObject[]{(DBObject)context};
+
+    boolean isUmsatzTypSelection = this.isUmsatzTypSelection(objects);
+    boolean hasAssignments = false;
+    if (isUmsatzTypSelection)
+    {
+      try
+      {
+        hasAssignments = this.hasAssignedUmsaetze(objects);
+      }
+      catch (Exception e)
+      {
+        Logger.error("unable to detect assignment count for delete warning",e);
+        hasAssignments = false;
+      }
+    }
+
     // Sicherheitsabfrage
     YesNoDialog d = new YesNoDialog(YesNoDialog.POSITION_CENTER);
     if (array)
     {
       d.setTitle(i18n.tr("Daten löschen"));
-      d.setText(i18n.tr("Wollen Sie diese {0} Datensätze wirklich löschen?",""+((DBObject[])context).length));
+      if (hasAssignments)
+        d.setText(i18n.tr("Es existieren Umsaetze zu mindestens einer der ausgewaehlten Kategorien. Wirklich loeschen?"));
+      else
+        d.setText(i18n.tr("Wollen Sie diese {0} Datensätze wirklich löschen?",""+objects.length));
     }
     else
     {
       d.setTitle(i18n.tr("Daten löschen"));
-      d.setText(i18n.tr("Wollen Sie diesen Datensatz wirklich löschen?"));
+      if (hasAssignments && objects[0] instanceof UmsatzTyp)
+      {
+        String name = this.getUmsatzTypName((UmsatzTyp) objects[0]);
+        d.setText(i18n.tr("Es existieren Umsaetze zu {0}. Wirklich loeschen?",name));
+      }
+      else
+      {
+        d.setText(i18n.tr("Wollen Sie diesen Datensatz wirklich löschen?"));
+      }
     }
     try {
       Boolean choice = (Boolean) d.open();
@@ -88,11 +125,7 @@ public class DBObjectDelete implements Action
       return;
     }
 
-    DBObject[] list = null;
-    if (array)
-      list = (DBObject[]) context;
-    else
-      list = new DBObject[]{(DBObject)context}; // Array mit einem Element
+    DBObject[] list = objects;
     
     Worker worker = new Worker(list);
 
@@ -102,7 +135,141 @@ public class DBObjectDelete implements Action
 //    else
     worker.run(null);
   }
+
+  private boolean isUmsatzTypSelection(DBObject[] objects)
+  {
+    if (objects == null || objects.length == 0)
+      return false;
+
+    for (DBObject o:objects)
+    {
+      if (!(o instanceof UmsatzTyp))
+        return false;
+    }
+
+    return true;
+  }
+
+  private boolean hasAssignedUmsaetze(DBObject[] objects) throws RemoteException
+  {
+    if (objects == null)
+      return false;
+
+    // Fast-path fuer den haeufigsten Fall: direkt markierte Kategorie hat Umsaetze.
+    for (DBObject o:objects)
+    {
+      UmsatzTyp typ = (UmsatzTyp) o;
+      if (this.hasAssignedUmsaetze(typ))
+        return true;
+    }
+
+    // Danach rekursiv auf Unterkategorien der Auswahl pruefen.
+    Set<String> ids = this.collectAffectedKategorieIds(objects);
+    for (String id:ids)
+    {
+      if (id == null)
+        continue;
+
+      UmsatzTyp typ = this.resolveUmsatzTyp(id);
+      if (typ == null)
+        continue;
+
+      if (this.hasAssignedUmsaetze(typ))
+        return true;
+    }
+
+    return false;
+  }
+
+  private boolean hasAssignedUmsaetze(UmsatzTyp typ) throws RemoteException
+  {
+    if (typ == null || typ.isNewObject())
+      return false;
+
+    return typ.getUmsaetze().hasNext();
+  }
+
+  private UmsatzTyp resolveUmsatzTyp(String id) throws RemoteException
+  {
+    if (id == null)
+      return null;
+
+    UmsatzTyp typ = (UmsatzTyp) de.willuhn.jameica.hbci.Settings.getDBService().createObject(UmsatzTyp.class,id);
+    if (typ == null || typ.isNewObject())
+      return null;
+
+    return typ;
+  }
+
+  private Set<String> collectAffectedKategorieIds(DBObject[] objects) throws RemoteException
+  {
+    Set<String> selected = new HashSet<String>();
+    for (DBObject o:objects)
+    {
+      UmsatzTyp typ = (UmsatzTyp) o;
+      String id = typ.getID();
+      if (id != null)
+        selected.add(id);
+    }
+
+    if (selected.isEmpty())
+      return selected;
+
+    DBIterator<UmsatzTyp> all = Settings.getDBService().createList(UmsatzTyp.class);
+    Map<String,List<String>> childrenByParent = new HashMap<String,List<String>>();
+    while (all.hasNext())
+    {
+      UmsatzTyp t = all.next();
+      String id = t.getID();
+      Object parent = t.getAttribute("parent_id");
+      if (id == null || parent == null)
+        continue;
+
+      String parentId = String.valueOf(parent);
+      List<String> children = childrenByParent.get(parentId);
+      if (children == null)
+      {
+        children = new LinkedList<String>();
+        childrenByParent.put(parentId,children);
+      }
+      children.add(id);
+    }
+
+    LinkedList<String> queue = new LinkedList<String>(selected);
+    while (!queue.isEmpty())
+    {
+      String current = queue.removeFirst();
+      List<String> children = childrenByParent.get(current);
+      if (children == null)
+        continue;
+
+      for (String child:children)
+      {
+        if (selected.add(child))
+          queue.add(child);
+      }
+    }
+
+    return selected;
+  }
   
+  private String getUmsatzTypName(UmsatzTyp typ)
+  {
+    if (typ == null)
+      return "";
+
+    try
+    {
+      String name = typ.getName();
+      return name != null ? name : "";
+    }
+    catch (Exception e)
+    {
+      Logger.error("unable to resolve category name for delete warning",e);
+      return "";
+    }
+  }
+
   /**
    * Damit koennen wir lange Loeschvorgaenge ggf. im Hintergrund laufen lassen
    */
